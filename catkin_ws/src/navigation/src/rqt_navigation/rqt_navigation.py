675c99fba5e8aec55dea7a6bec90e4fe3eeb77c6
franzoni <franzoni315@gmail.com>
2016-04-08 18:11:01 -0400

    Added RQT Plugin Template

---
+++ catkin_ws/src/navigation/src/rqt_navigation/rqt_navigation.py
@@ -0,0 +1,51 @@
+import os
+import rospy
+
+from qt_gui.plugin import Plugin
+from python_qt_binding import loadUi
+from python_qt_binding.QtGui import QWidget
+
+class RQTNavigation(Plugin):
+
+    def __init__(self, context):
+        super(RQTNavigation, self).__init__(context)
+        # Give QObjects reasonable names
+        self.setObjectName('Navigation')
+
+        # Create QWidget
+        self._widget = QWidget()
+        # Get path to UI file which is a sibling of this file
+        # in this example the .ui and .py file are in the same folder
+        ui_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'rqt_navigation.ui')
+        # Extend the widget with all attributes and children from UI file
+        loadUi(ui_file, self._widget)
+        # Give QObjects reasonable names
+        self._widget.setObjectName('rqt_navigation')
+        # Show _widget.windowTitle on left-top of each plugin (when 
+        # it's set in _widget). This is useful when you open multiple 
+        # plugins at once. Also if you open multiple instances of your 
+        # plugin at once, these lines add number to make it easy to 
+        # tell from pane to pane.
+        if context.serial_number() > 1:
+            self._widget.setWindowTitle(self._widget.windowTitle() + (' (%d)' % context.serial_number()))
+        # Add widget to the user interface
+        context.add_widget(self._widget)
+
+    def shutdown_plugin(self):
+        # TODO unregister all publishers here
+        pass
+
+    def save_settings(self, plugin_settings, instance_settings):
+        # TODO save intrinsic configuration, usually using:
+        # instance_settings.set_value(k, v)
+        pass
+
+    def restore_settings(self, plugin_settings, instance_settings):
+        # TODO restore intrinsic configuration, usually using:
+        # v = instance_settings.value(k)
+        pass
+
+    #def trigger_configuration(self):
+        # Comment in to signal that the plugin has a way to configure
+        # This will enable a setting button (gear icon) in each dock widget title bar
+        # Usually used to open a modal configuration dialog
