f5a5ea7d0fcdf765af11950e044daded4ed7752b
Hang <zhaohang0124@gmail.com>
2016-04-10 13:45:40 -0400

    slected images for annotation

--- catkin_ws/src/f1/anti_instagram/scripts/annotation/run_road_annotator.m
+++ catkin_ws/src/f1/anti_instagram/scripts/annotation/run_road_annotator.m
@@ -1,3 +1,12 @@
-file_list={'agirard/frame0000.jpg','agirard/frame0001.jpg','agirard/frame0002.jpg'};
+clear;
+addpath ./images/
+
+listing = dir('images/*.jpg');
+file_list = cell(numel(listing), 1);
+for i = 1:numel(listing)
+    file_list{i} = ['images/', listing(i).name];
+end
+
 road_annotation(file_list);
-load('my_saved_file.mat','ud')
+
+%load('my_saved_file.mat','ud')
