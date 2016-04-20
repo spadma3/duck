bbc2031e2e236ad071b201a1a5e6bcc67c7d2aff
Liam Paull Test <lpaull@mit.edu>
2016-04-20 03:49:00 +0000

    tuning open loop with parameterization

--- catkin_ws/src/intersection_control/src/open_loop_intersection_control_node.py
+++ catkin_ws/src/intersection_control/src/open_loop_intersection_control_node.py
@@ -11,7 +11,7 @@ class OpenLoopIntersectionNode(object):
         self.node_name = rospy.get_name()
         self.mode = None
         self.turn_type = -1
-        self.in_lane = False
+#        self.in_lane = False
         self.lane_pose = LanePose()
         self.stop_line_reading = StopLineReading()
 
@@ -34,7 +34,7 @@ class OpenLoopIntersectionNode(object):
         self.rate = rospy.Rate(30)
 
         # Subscribers
-        self.sub_in_lane = rospy.Subscriber("~in_lane", BoolStamped, self.cbInLane, queue_size=1)
+#        self.sub_in_lane = rospy.Subscriber("~in_lane", BoolStamped, self.cbInLane, queue_size=1)
         self.sub_turn_type = rospy.Subscriber("~turn_type", Int16, self.cbTurnType, queue_size=1)
         self.sub_mode = rospy.Subscriber("~mode", FSMState, self.cbFSMState, queue_size=1)
         self.sub_lane_pose = rospy.Subscriber("~lane_pose", LanePose, self.cbLanePose, queue_size=1)
@@ -81,6 +81,7 @@ class OpenLoopIntersectionNode(object):
         if (not self.mode == "INTERSECTION_CONTROL") and msg.state == "INTERSECTION_CONTROL":
             # Switch into INTERSECTION_CONTROL mode
             rospy.loginfo("[%s] %s triggered." %(self.node_name,self.mode))
+            rospy.loginfo("updating trajectory: distance from stop_line=%s, lane_pose_phi = %s", self.stop_line_reading.stop_line_point.x,  self.lane_pose.phi)
             start = rospy.Time.now()
             current = rospy.Time.now()
             while current.secs - start.secs < 0.5:
@@ -103,8 +104,7 @@ class OpenLoopIntersectionNode(object):
         car_cmd   = first_leg[1];
         new_exec_time = exec_time + self.stop_line_reading.stop_line_point.x/car_cmd.v
         rospy.loginfo("old exec_time = %s, new_exec_time = %s" ,exec_time, new_exec_time)
-        ###### warning this next line is because of wrong inverse kinematics - remove the 10s after it's fixed
-        new_car_cmd = Twist2DStamped(v=car_cmd.v,omega=10*(car_cmd.omega/10 - self.lane_pose.phi/new_exec_time))
+        new_car_cmd = Twist2DStamped(v=car_cmd.v,omega=(car_cmd.omega - self.lane_pose.phi/new_exec_time))
         new_first_leg = [new_exec_time,new_car_cmd]
         print "old car command"
         print car_cmd
@@ -126,6 +126,8 @@ class OpenLoopIntersectionNode(object):
         for index, pair in enumerate(self.maneuvers[turn_type]):
             cmd = copy.deepcopy(pair[1])
             start_time = rospy.Time.now()
+            print "executing:"
+            print pair
             end_time = start_time + rospy.Duration.from_sec(pair[0])
             while rospy.Time.now() < end_time:
                 if not self.mode == "INTERSECTION_CONTROL": # If not in the mode anymore, return
@@ -134,7 +136,7 @@ class OpenLoopIntersectionNode(object):
                 self.pub_cmd.publish(cmd)
                 if index > 1:
                     # See if need to publish interesction_done
-                    if self.in_lane and not (published_already):
+                    if  not (published_already):
                         published_already = True
                         self.publishDoneMsg()
                         return
