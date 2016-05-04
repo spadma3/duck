05ccb4c67c27531a49c103a29d55765f7c8b3bfc
Robert Katzschmann <rkk@csail.mit.edu>
2016-05-04 14:07:42 -0400

    adjust all names within the package to be now called vehicle_follow

--- catkin_ws/src/vehicle_follow/setup.py
+++ catkin_ws/src/vehicle_follow/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['parallel_autonomy'],
+    packages=['vehicle_follow'],
     package_dir={'': 'include'},
 )
 
