73abd87686686c9ba80566e9accc7ae89dbae9d2
Valerio <valerio@mit.edu>
2016-05-11 03:32:38 +0000

    WIP on fixing build

--- catkin_ws/src/street_name_detector/src/mser.cpp
+++ catkin_ws/src/street_name_detector/src/mser.cpp
@@ -17,7 +17,7 @@
 // <http://www.gnu.org/licenses/>.
 //--------------------------------------------------------------------------------------------------
 
-#include "mser.h"
+#include "street_name_detector/mser.h"
 
 #include <algorithm>
 #include <cassert>
