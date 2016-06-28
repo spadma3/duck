0b7306515c20c212667892cdc4de4c153d066c17
premphy <premphy@mit.edu>
2016-06-28 11:29:12 -0400

    Trainig data for the cascade classifier, 32X32 window

--- vehicle-detection-test/scripts/train_samples.sh
+++ vehicle-detection-test/scripts/train_samples.sh
@@ -1,8 +1,8 @@
 opencv_traincascade -data ~/duckietown/vehicle-detection-test/classifier \
-	-featureType HAAR \
+	-featureType LBP \
 	-vec ~/duckietown/vehicle-detection-test/samples/samples.vec \
     -bg ~/duckietown/vehicle-detection-test/negatives.dat \
-    -numPos 57 -numNeg 43 -numStages 10 \
-    -precalcValBufSize 100 -precalcIdxBufSize 100 \
+    -numPos 57 -numNeg 8 -numStages 20 \
+    -precalcValBufSize 500 -precalcIdxBufSize 500 \
     -minHitRate 0.999 -maxFalseAlarmRate 0.5 -mode ALL \
     -w 300 -h 300
