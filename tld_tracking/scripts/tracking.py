b569ba9d915ba83b9bb663c0095351860f3faefe
premphy <premphy@mit.edu>
2016-07-15 10:29:27 -0400

    Debugging

--- tld_tracking/scripts/tracking.py
+++ tld_tracking/scripts/tracking.py
@@ -5,10 +5,11 @@ from time import time
 
 class Tracker():
 	def __init__(self, video_path=None):
-		self.bounding_box = [(30,30), (100,100)]
+		self.bounding_box = [(300.0,100.0), (370.0,170.0)]
+		# self.bounding_box = [(30,30), (100,100)]
 		self.creating_bounding_box = False
-		self.init_pts_density = 4
-		self.margin = 2
+		self.init_pts_density = 40
+		self.margin = 0.0
 		self.start_img = None
 		self.target_img = None
 		self.viz = None
@@ -17,6 +18,7 @@ class Tracker():
 		else:
 			self.video = cv2.VideoCapture(video_path)
 		self.start()
+		self.start_pts = self.gen_point_cloud(self.bounding_box)
 		self.tracking()
 
 
@@ -48,20 +50,50 @@ class Tracker():
 				break
 
 	def tracking(self):
+		prev_frame = 0
 		while True:
 			start_bb = self.bounding_box
+			self.video.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, 2 + prev_frame)
 			flag, self.viz = self.video.read()
-			print self.video.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+			prev_frame = self.video.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+			print prev_frame
+			# print self.video.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+			# print ""
 			self.target_img = cv2.cvtColor(self.viz, cv2.COLOR_BGR2GRAY)
-			start_pts = self.gen_point_cloud(start_bb)  #[(x,y),(x,y)...]
-			corr, dist, target_pts, start_pts = self.cal_target_pts(start_pts)
-			good_start_pts, good_target_pts = self.filter_pts(corr, dist, target_pts, start_pts)
+			# print "Target_img gray generated"
+			cv2.namedWindow("Start and Target Image")
+			cv2.imshow("Start and Target Image", self.start_img)
+			cv2.waitKey(500)
+			cv2.imshow("Start and Target Image", self.target_img)
+			cv2.waitKey(500)
+			self.start_pts = self.gen_point_cloud(start_bb)
+			print self.start_pts
+			print ""
+			corr, dist, valid_target_pts, valid_start_pts = self.cal_target_pts(self.start_pts)
+			# print "Predicted target and start points"
+			print valid_target_pts
+			print ""
+			print valid_start_pts
+			print ''
+			good_target_pts, good_start_pts = self.filter_pts(corr, dist, valid_target_pts, valid_start_pts)
+			# print "Filtered to get good points"
+			print good_start_pts
+			print ""
+			print  good_target_pts
+			print ''
 			self.bounding_box = self.target_bounding_box(start_bb,good_start_pts, good_target_pts)
 			print self.bounding_box
 			cv2.rectangle(self.viz, self.bounding_box[0], self.bounding_box[1], (0,255,0),1)
+			# print "Drew the rectangle"
 			self.start_img = self.target_img
+			# if len(good_target_pts) < 200:
+			# 	self.start_pts = self.gen_point_cloud(self.bounding_box)
+			# else:
+			# 	self.start_pts = good_target_pts
+			# print "Reassigned"
 			cv2.imshow("Tracking", self.viz)
-			cv2.waitKey(10)
+			# print "Shown the image"
+			cv2.waitKey(1000)
 
 	def create_bounding_box(self,event, x, y, flags, param):
 		self.show_window()
@@ -83,8 +115,8 @@ class Tracker():
 		pts = [] 		# [(x1,y1),(x2,y2)...]
 		img_track = [(box[0][0]+self.margin, box[0][1]+self.margin), \
 		(box[1][0]-self.margin,box[1][0]-self.margin)]
-		numY = ((img_track[1][1] - img_track[0][1])/self.init_pts_density) + 1
-		numX = (img_track[1][0] - img_track[0][0])/self.init_pts_density + 1
+		numY = int(((img_track[1][1] - img_track[0][1])/self.init_pts_density)) + 1
+		numX = int((img_track[1][0] - img_track[0][0])/self.init_pts_density) + 1
 		for i in range(numX):
 			for j in range(numY):
 				pts_x = box[0][0] + i*self.init_pts_density
@@ -98,12 +130,12 @@ class Tracker():
 		start_pts = np.asarray(pts0, dtype="float32")
 		target_pts = np.asarray(pts0, dtype="float32")
 		back_pts = np.asarray(pts0, dtype="float32")
-		lk_params = dict(winSize=(15,15), maxLevel=2, criteria=(cv2.TERM_CRITERIA_EPS | \
+		lk_params = dict(winSize=(4,4), maxLevel=2, criteria=(cv2.TERM_CRITERIA_EPS | \
 			cv2.TERM_CRITERIA_COUNT,10,0.03),flags=cv2.OPTFLOW_USE_INITIAL_FLOW)
-		matching_param = dict(winSize_match=4, method=cv2.cv.CV_TM_CCOEFF_NORMED)
+		matching_param = dict(winSize_match=10, method=cv2.cv.CV_TM_CCOEFF_NORMED)
 
 		target_pts, status_forward,_ = cv2.calcOpticalFlowPyrLK(self.start_img,self.target_img,start_pts,target_pts,**lk_params) 
-		back_pts, status_backward,_ = cv2.calcOpticalFlowPyrLK(self.start_img,self.target_img,target_pts,back_pts,**lk_params)
+		back_pts, status_backward,_ = cv2.calcOpticalFlowPyrLK(self.target_img,self.start_img,target_pts,back_pts,**lk_params)
 		status = status_forward & status_backward
 
 		dist_all = self.euclidean_distance(start_pts, target_pts)
@@ -112,8 +144,8 @@ class Tracker():
 
 		for i in np.argwhere(status):
 			i = i[0]
-			valid_target_pts.append(target_pts[i])
-			valid_start_pts.append(start_pts[i])
+			valid_target_pts.append(tuple(target_pts[i].tolist()))
+			valid_start_pts.append(tuple(start_pts[i].tolist()))
 			valid_dist.append(dist_all[i])
 
 		test = len(valid_start_pts) == len(valid_target_pts) == len(valid_dist) == len(valid_corr)
@@ -140,7 +172,7 @@ class Tracker():
 		for i in range(len(valid_dist)):
 			if valid_dist[i] <= medDist and valid_corr[i] >= medCorr:
 				good_target_points.append(valid_target_pts[i])
-				good_start_points.append(valid_target_pts[i])
+				good_start_points.append(valid_start_pts[i])
 		return good_target_points, good_start_points
 
 	def target_bounding_box(self,start_box,good_start_points, good_target_points):
@@ -156,6 +188,8 @@ class Tracker():
 		dy = self.median(diff_y)
 		diff_y = diff_x = 0
 
+		print dx, dy, "The shift from distance"
+
 		scale_factor = []
 		for i in range(num_target_pts):
 			for j in range(i+1, num_target_pts):
@@ -166,9 +200,8 @@ class Tracker():
 				scale_factor.append(float(target_img)/start_img)
 		
 		scale = self.median(scale_factor)
-		
-		if scale == 0:
-			scale = 1
+
+		print scale, "The scale change"
 		scale_x = ((scale -1)/2)*width_start
 		scale_y = ((scale-1)/2)*height_start
 
@@ -177,7 +210,7 @@ class Tracker():
 		y1_new = start_box[0][1] + dy - scale_y
 		y2_new = start_box[1][1] + dy + scale_y
 
-		target_box = [(int(x1_new),int(y1_new)), (int(x2_new), int(y2_new))]
+		target_box = [(int(round(x1_new)),int(round(y1_new))), (int(round(x2_new)), int(round(y2_new)))]
 		return target_box
 
 	def median(self,data):
