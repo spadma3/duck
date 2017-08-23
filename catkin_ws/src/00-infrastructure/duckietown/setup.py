804b1deac65c6e27873a985f05d96bbe6a9c7ec0
Andrea Censi <acensi@idsc.mavt.ethz.ch>
2017-08-23 22:25:34 +0200

    test

--- catkin_ws/src/00-infrastructure/duckietown/setup.py
+++ catkin_ws/src/00-infrastructure/duckietown/setup.py
@@ -4,9 +4,12 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
     packages=[
-	   'duckietown_utils',
+        'duckietown_utils',
+        'duckietown_utils_tests',
     ],
-    install_requires=[],
+    install_requires=[
+        'comptests',
+    ],
     package_dir={'': 'include'},
 )
 setup(**setup_args)
--- catkin_ws/src/00-infrastructure/easy_logs/setup.py
+++ catkin_ws/src/00-infrastructure/easy_logs/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=['easy_logs', 'easy_logs'],
+    packages=['easy_logs', 'easy_logs_tests'],
     package_dir={'': 'include'},
     install_requires=['procgraph']
 )
