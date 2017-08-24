52b753115d35003440f791e5ff21a458c193f3b7
Andrea Censi <AndreaCensi@users.noreply.github.com>
2017-08-24 16:36:29 +0200

    Update subscriber_node.py

--- catkin_ws/src/60-templates/pkg_name/src/subscriber_node.py
+++ catkin_ws/src/60-templates/pkg_name/src/subscriber_node.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import String #Imports msg
+
+# Imports message type
+from std_msgs.msg import String 
 
 # Define callback function
 def callback(msg):
-    rospy.loginfo("I heard: %s" %(msg.data))
+    s = "I heard: %s" % (msg.data)
+    rospy.loginfo(s)
 
 # Initialize the node with rospy
 rospy.init_node('subscriber_node', anonymous=False)
+
 # Create subscriber
 subscriber = rospy.Subscriber("topic", String, callback)
 
-rospy.spin() #Keeps the script for exiting
+# Runs continuously until interrupted
+rospy.spin() 
