05f434352e8ef9578a3e042276f8ab22abff28a0
Andrea Censi <acensi@idsc.mavt.ethz.ch>
2017-08-25 16:53:47 +0200

    Docker

--- Makefiles/Makefile.docker.mk
+++ Makefiles/Makefile.docker.mk
@@ -10,7 +10,7 @@ docker:
 
 docker_dir=.circleci/images/duckietown-xenial-kinetic/
 docker_image_name=andreacensi/duckietown-xenial-kinetic
-tag=13
+tag=14
 
 docker-build:
 	sudo docker build -t $(docker_image_name):$(tag) $(docker_dir)
