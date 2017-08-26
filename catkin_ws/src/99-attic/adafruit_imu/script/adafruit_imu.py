efee31b74da03761a0e9dec486d4ae80ba180e02
Andrea Censi <AndreaCensi@users.noreply.github.com>
2017-08-26 14:57:42 +0200

    Some updates (#169)
    * Vim configuration
    
    * Clean up
    
    * readme

--- catkin_ws/src/99-attic/adafruit_imu/script/adafruit_imu.py
+++ catkin_ws/src/99-attic/adafruit_imu/script/adafruit_imu.py
@@ -50,26 +50,26 @@ class AdafruitIMU(object):
         gyro = self.gyro.read()
         
         # Put together an IMU message
-	imu_msg = Imu()
-	imu_msg.header.stamp = rospy.Time.now()
+        imu_msg = Imu()
+        imu_msg.header.stamp = rospy.Time.now()
         imu_msg.orientation_covariance[0] = -1
         imu_msg.angular_velocity = gyro[0] * DEG_TO_RAD
         imu_msg.angular_velocity = gyro[1] * DEG_TO_RAD
         imu_msg.angular_velocity = gyro[2] * DEG_TO_RAD
-	imu_msg.linear_acceleration.x = accel[0] * G
-	imu_msg.linear_acceleration.y = accel[1] * G
-	imu_msg.linear_acceleration.z = accel[2] * G
+        imu_msg.linear_acceleration.x = accel[0] * G
+        imu_msg.linear_acceleration.y = accel[1] * G
+        imu_msg.linear_acceleration.z = accel[2] * G
     
-	self.pub_imu.publish(imu_msg)
+        self.pub_imu.publish(imu_msg)
 
         # Put together a magnetometer message
-	mag_msg = MagneticField()
-	mag_msg.header.stamp = rospy.Time.now()
+        mag_msg = MagneticField()
+        mag_msg.header.stamp = rospy.Time.now()
         mag_msg.magnetic_field.x = compass[0]
         mag_msg.magnetic_field.y = compass[1]
         mag_msg.magnetic_field.z = compass[2]
     
-	self.pub_mag.publish(mag_msg)
+        self.pub_mag.publish(mag_msg)
 
 if __name__ == "__main__":
     rospy.init_node("Adafruit_IMU", anonymous=False)
