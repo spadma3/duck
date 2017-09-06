db9a4eb491ae8bf5af3f9d1c562e0f2faad47a94
YuehChuan <schwarmcyc@hotmail.com>
2017-09-07 01:35:11 +0800

    fix

--- dependencies_since_image.sh
+++ dependencies_since_image.sh
@@ -2,4 +2,6 @@
 set -e
 set -x
 sudo apt install -y python-frozendict python-ruamel.yaml
-pip install --upgrade --user PyContracts QuickApp conftools comptests procgraph
+sudo pip install --upgrade --user PyContracts QuickApp conftools comptests procgraph
+sudo pip install setuptools wheel
+sudo pip install --upgrade ruamel.yaml
