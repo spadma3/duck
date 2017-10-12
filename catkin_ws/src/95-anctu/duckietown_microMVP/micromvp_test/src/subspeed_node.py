d12283f8a57b0b28845d0d3f5b5e48362e69a60f
eric565648 <eric565648@gmail.com>
2017-10-12 11:02:03 +0800

    subspeed move

--- catkin_ws/src/95-anctu/duckietown_microMVP/micromvp_test/src/subspeed_node.py
+++ catkin_ws/src/95-anctu/duckietown_microMVP/micromvp_test/src/subspeed_node.py
@@ -1,20 +1,47 @@
 #!/usr/bin/env python
-
 import rospy
 import rospkg
 from micromvp_test.msg import micromvp_carspeed
+from dagu_car.dagu_wheels_driver import DaguWheelsDriver
 
-def callback(data):
-	print "Enter callback", data.carID, "lspeed: ", data.lspeed
+class Subspeed_node(object):
+	def __init__(self):
+		self.node_name = rospy.get_name()
+		rospy.loginfo("[%s] Initializing " %(self.node_name))
 
-def Subspeed_node():
-	rospy.init_node('subspeed', anonymous=True)
-	
-	rospy.Subscriber('micromvp', micromvp_carspeed, callback, queue_size=20)
+		# Open driver
+		self.driver = DaguWheelsDriver()
 
-	print "Enter subspeed_node"
+		# Setup subscribers
+		self.control_constant = 1.0
+		self.sub_topic = rospy.Subscriber("~wheels_cmd", micromvp_carspeed, self.cbWheelsCmd, queue_size=1)
 
+		self.max_vel = 200
+
+	def cbWheelsCmd(self,msg):
+		
+		lspeed = msg.lspeed * self.max_vel
+		rspeed = msg.rspeed * self.max_vel #change normalize to real pwm
+		self.driver.setWheelsSpeed(left=lspeed,right=rspeed)
+		
+		# Put the wheel commands in a message and publish
+		#self.msg_wheels_cmd.header = msg.header
+		# Record the time the command was given to the wheels_driver
+		#self.msg_wheels_cmd.header.stamp = rospy.get_rostime()  
+		#self.msg_wheels_cmd.vel_left = msg.vel_left
+		#self.msg_wheels_cmd.vel_right = msg.vel_right
+		#self.pub_wheels_cmd.publish(self.msg_wheels_cmd)
+
+	def on_shutdown(self):
+		self.driver.setWheelsSpeed(left=0.0,right=0.0)
+		rospy.loginfo("[%s] Shutting down."%(rospy.get_name()))
+
+if __name__ == '__main__':
+	# Initialize the node with rospy
+	rospy.init_node('subspeed_node', anonymous=False)
+	# Create the DaguCar object
+	node = Subspeed_node()
+	# Setup proper shutdown behavior 
+	rospy.on_shutdown(node.on_shutdown)
+	# Keep it spinning to keep the node alive
 	rospy.spin()
-
-if __name__ == '__main__': 
-	Subspeed_node()
