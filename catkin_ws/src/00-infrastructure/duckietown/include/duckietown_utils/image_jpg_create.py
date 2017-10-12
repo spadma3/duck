afe5a335e73426cb01c79c6533a2310eb25a30bc
Andrea F. Daniele <afdaniele@ttic.edu>
2017-10-12 18:41:01 -0500

    Broken import fixed

--- catkin_ws/src/00-infrastructure/duckietown/include/duckietown_utils/image_jpg_create.py
+++ catkin_ws/src/00-infrastructure/duckietown/include/duckietown_utils/image_jpg_create.py
@@ -1,7 +1,7 @@
 
 
 
-def d8_compressed_image_from_cv_image(image_cv):
+def d8_compressed_image_from_cv_image(image_cv, same_timestamp_as = None):
     """ 
         Create CompressedIamge from a CV image.
     
@@ -9,11 +9,15 @@ def d8_compressed_image_from_cv_image(image_cv):
     """
     import cv2
     import numpy as np
-    from sensor_msgs import CompressedImage  # @UnresolvedImport
+    from sensor_msgs.msg import CompressedImage
     compress =  cv2.imencode('.jpg', image_cv)[1]
     jpg_data = np.array(compress).tostring()
     msg = CompressedImage()
-    msg.header.stamp = rospy.Time.now()
+    if same_timestamp_as is not None:
+        msg.header.stamp = same_timestamp_as.header.stamp
+    else:
+        from rospy import Time
+        msg.header.stamp = Time.now()
     msg.format = "jpeg"
     msg.data = jpg_data
     return msg
