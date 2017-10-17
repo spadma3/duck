db531187618cb2dd7dd80bebd4070c9f34ef6ff4
Andrea Censi <acensi@idsc.mavt.ethz.ch>
2017-10-17 08:50:02 +0200

    More development

--- catkin_ws/src/00-infrastructure/what_the_duck/include/what_the_duck/suite_git.py
+++ catkin_ws/src/00-infrastructure/what_the_duck/include/what_the_duck/suite_git.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from what_the_duck.checks.existence import FileExists
-from what_the_duck.entry import Diagnosis
 from what_the_duck.checks.file_contains import FileContains
+from what_the_duck.entry import Diagnosis, SeeDocs
+
 
 GIT_CONFIG = '~/.gitconfig'
 
@@ -16,14 +17,17 @@ def add_suite_git(manager):
     add(gitconfig,
         "Git config: email",
         FileContains(GIT_CONFIG, "email ="),
-        Diagnosis("You did not configure your email for Git."))
+        Diagnosis("You did not configure your email for Git."),
+        SeeDocs('howto-git-local-config'))
 
     add(gitconfig,
         "Git config: name ",
         FileContains(GIT_CONFIG, "name ="),
-        Diagnosis("You did not configure your name for Git."))
+        Diagnosis("You did not configure your name for Git."),
+        SeeDocs('howto-git-local-config'))
 
     add(gitconfig,
         "Git config: push policy",
         FileContains(GIT_CONFIG, "[push]"),
-        Diagnosis("You did not configure the push policy for Git."))
+        Diagnosis("You did not configure the push policy for Git."),
+        SeeDocs('howto-git-local-config'))
