69ebbabe8976d90548a3e3ab2b644edd1766178f
Andrea Censi <acensi@idsc.mavt.ethz.ch>
2017-10-19 10:36:27 +0200

    Better way to install package

--- dependencies_since_image.sh
+++ dependencies_since_image.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 set -e
 set -x
-sudo apt install -y python-frozendict libxslt-dev libxml2-dev
+
+sudo apt install -y \
+	python-frozendict \
+	libxslt-dev \
+	libxml2-dev \
+	python-lxml \
+	python-bs4 \
+	python-ruamel.yaml \
+	python-ruamel.ordereddict \
+	python-pymongo
+
 pip install --upgrade --user \
-	ruamel.yaml \
-	bs4 \
-	lxml \
 	PyContracts \
 	QuickApp \
 	conftools \
 	comptests \
-	procgraph \
-	pymongo
+	procgraph
 
 # None of this should be needed. Next time you think you need it, let me know and we figure it out. -AC
-# sudo pip install --upgrade pip setuptools wheel 
+# sudo pip install --upgrade pip setuptools wheel
