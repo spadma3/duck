293ea959f903f1d8f3f32497ff4d38a7d6f8b237
Liam Paull <paulll@iro.umontreal.ca>
2017-11-07 22:41:58 +0100

    not using anymore

--- log_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ ! -d '/media/logs' ]; then
-    echo "USB not mounted.. mounting to /media/logs ... exiting"
-    exit
-fi
-echo -n "institution? (in {UdM,ETHZ,NCTU,TTIC}): "
-read INSTITUTION
-export $INSTITUTION
