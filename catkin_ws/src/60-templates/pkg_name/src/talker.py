8886cc4c25c311853a476a142c7ce11693338fe4
AnnaDaiZH <adai@student.ethz.ch>
2017-11-10 18:32:01 +0100

    update talker

--- catkin_ws/src/60-templates/pkg_name/src/talker.py
+++ catkin_ws/src/60-templates/pkg_name/src/talker.py
@@ -2,6 +2,10 @@
 import rospy
 from pkg_name.util import HelloGoodbye #Imports module. Not limited to modules in this pkg. 
 from std_msgs.msg import String #Imports msg
+#from sensor_msgs.msg import CameraInfo, CompressedImage, Image
+#from duckietown_utils.jpg import image_cv_from_jpg
+#from cv_bridge import CvBridge  
+from sys import argv
 
 class Talker(object):
     def __init__(self):
@@ -12,11 +16,15 @@ class Talker(object):
 
         # Setup publishers
         self.pub_topic_a = rospy.Publisher("~topic_a",String, queue_size=1)
+        #self.pub_img = rospy.Publisher("/talker/filter/compressed",Image,queue_size=1)
         # Setup subscriber ,"" has to be equal to launch file channel
-        self.sub_topic_b = rospy.Subscriber("~topic_b", String, self.cbTopic) 
-        # Read parameters
+        self.sub_topic_b = rospy.Subscriber("~topic_b", String, self.cbTopic)
+		# Read parameters
         print("Hi a")
         self.pub_timestep = self.setupParameter("~pub_timestep",1.0)
+       	self.filter=self.setupParameter("~filter", "x")
+       	filter_st=self.filter.value
+       	print("filter_st " + filter_st)
         # change inside "" will be displayed at the beginning in terminal
         # Create a timer that calls the cbTimer function every 1.0 second
         print("hi b")
@@ -55,6 +63,7 @@ if __name__ == '__main__':
     # Create the NodeName object
     node = Talker()
 
+    #filters = argv[3]
     # Setup proper shutdown behavior 
     rospy.on_shutdown(node.on_shutdown)
     
