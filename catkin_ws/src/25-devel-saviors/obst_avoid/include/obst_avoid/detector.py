42bfbe5715189a0ca0185451feef458fc4c2d408
Niklas Funk <nfunk@ethz.ch>
2017-11-27 21:00:03 +0100

    added first devel-saviors files

---
+++ catkin_ws/src/25-devel-saviors/obst_avoid/include/obst_avoid/detector.py
@@ -0,0 +1,252 @@
+#!/bin/bash/env python
+
+import argparse
+import cv2
+import numpy as np
+from numpy.linalg import inv
+from os.path import basename, expanduser, isfile, join, splitext
+import socket
+from matplotlib import pyplot as plt
+import time
+
+import rospy
+from sensor_msgs.msg import CompressedImage
+
+from duckietown_utils import d8_compressed_image_from_cv_image, logger, rgb_from_ros, yaml_load, get_duckiefleet_root
+from duckietown_utils import get_base_name, load_camera_intrinsics, load_homography, load_map, rectify
+from duckietown_utils import load_map, load_camera_intrinsics, load_homography, rectify
+
+class Detector():
+    '''class for detecting obstacles'''
+    def __init__(self, robot_name='', map_file=''):
+        # Robot name
+        self.robot_name = robot_name
+
+        # Load camera calibration parameters
+	self.intrinsics = load_camera_intrinsics(robot_name)
+	self.H = inv(load_homography(self.robot_name))	
+	
+    def process_image(self, image):
+
+    # YOUR CODE GOES HERE, image is BGR
+	# CROP IMAGE
+ 	im1_cropped = image[130:,:,:]
+
+    # FILTER IMAGE
+	# Convert BGR to HSV
+	hsv = cv2.cvtColor(im1_cropped, cv2.COLOR_RGB2HSV)
+	lower_yellow = np.array([20,75,100])
+	upper_yellow = np.array([40,255,255])
+	# Threshold the HSV image to get only yellow colors
+	mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+	#die Randpixel auf null setzen sonst unten probleme mit den boundaries
+	mask[0,:]=0
+	mask[np.size(mask,0)-1,:]=0
+	mask[:,0]=0
+	mask[:,np.size(mask,1)-1]=0
+	
+	if(np.sum(mask!=0)!=0): #there were segment detected then
+		#SEGMENT IMAGE
+		segmented_image=self.segment_img(mask)
+
+		#apply filter on elements-> only obstacles remain and mark them in original picture
+		#in the future: might be separated in 2 steps 1)extract objects 2)visualisation		
+		image = self.object_filter(segmented_image,im1_cropped)
+	else:
+		image = im1_cropped
+
+	# CHANGE BGR BACK TO RGB
+	jpg_data = image[:,:,::-1]
+	#before sending: REDEFINE DATA 
+    	return d8_compressed_image_from_cv_image(jpg_data)
+
+
+    def segment_img(self, image):
+		#returns segmented image on Grayscale image where all interconnected pixels have same number
+
+	    A=np.nonzero(image)
+	    nonzero_val=np.empty(np.shape(A),dtype=int) #read the nonzero values
+	    nonzero_val[0,:]=A[0] 
+	    nonzero_val[1,:]=A[1]
+
+	    end_arr=np.size(nonzero_val,1)-1 
+
+	    final=np.zeros_like(image)
+	    i=1
+	    final[nonzero_val[0,end_arr],nonzero_val[1,end_arr]]=i
+	    #merged=0
+	    check = False
+
+	    #SEGMENTIERUNG MUSS DEFINITIV INTELLIGENTER WERDEN!!!! -> BISHER NOCH VIEL ZU VIELE!!!! SHIIIIIT^^
+	    for l in range(1,np.size(A,1)):
+		j=end_arr-l
+
+		if (final[nonzero_val[0,j]+1,nonzero_val[1,j]]!=0): #eins nach unten
+		    final[nonzero_val[0,j],nonzero_val[1,j]]=final[nonzero_val[0,j]+1,nonzero_val[1,j]]
+		    if (check and (final[nonzero_val[0,j],nonzero_val[1,j]]!=final[nonzero_val[0,j],nonzero_val[1,j]+1] and final[nonzero_val[0,j],nonzero_val[1,j]+1]!=0)):
+			#akt WERT final[nonzero_val[0,j],nonzero_val[1,j]] anders als der 1ns rechts final[nonzero_val[0,j],nonzero_val[1,j]+1] und der rechte !=0!!!
+			# -> der rechte WERT IST FALSCH!!! WEISSE DEN AKTUELLEN ZU!!
+			final[final == final[nonzero_val[0,j],nonzero_val[1,j]+1]] = final[nonzero_val[0,j],nonzero_val[1,j]] #
+			check = False 
+			#merged+=1
+
+
+		elif (final[nonzero_val[0,j]+1,nonzero_val[1,j]+1]!=0): #eins rechts und unten
+		    final[nonzero_val[0,j],nonzero_val[1,j]]=final[nonzero_val[0,j]+1,nonzero_val[1,j]+1]
+
+		    if (check and (final[nonzero_val[0,j],nonzero_val[1,j]]!=final[nonzero_val[0,j],nonzero_val[1,j]+1] and final[nonzero_val[0,j],nonzero_val[1,j]+1]!=0)):
+			#akt WERT final[nonzero_val[0,j],nonzero_val[1,j]] anders als der 1ns rechts final[nonzero_val[0,j],nonzero_val[1,j]+1] und der rechte !=0!!!
+			# -> der rechte WERT IST FALSCH!!! WEISSE DEN AKTUELLEN ZU!!
+			final[final == final[nonzero_val[0,j],nonzero_val[1,j]+1]] = final[nonzero_val[0,j],nonzero_val[1,j]] #
+			check = False
+			#merged+=1
+
+
+		if (final[nonzero_val[0,j]+1,nonzero_val[1,j]-1]!=0): #eins nach unten & links
+		    final[nonzero_val[0,j],nonzero_val[1,j]]=final[nonzero_val[0,j]+1,nonzero_val[1,j]-1]
+		    if (check and (final[nonzero_val[0,j],nonzero_val[1,j]]!=final[nonzero_val[0,j],nonzero_val[1,j]+1] and final[nonzero_val[0,j],nonzero_val[1,j]+1]!=0)):
+			#akt WERT final[nonzero_val[0,j],nonzero_val[1,j]] anders als der 1ns rechts final[nonzero_val[0,j],nonzero_val[1,j]+1] und der rechte !=0!!!
+			# -> der rechte WERT IST FALSCH!!! WEISSE DEN AKTUELLEN ZU!!
+			final[final == final[nonzero_val[0,j],nonzero_val[1,j]+1]] = final[nonzero_val[0,j],nonzero_val[1,j]] #
+			check = False
+			#merged+=1
+
+		elif(final[nonzero_val[0,j],nonzero_val[1,j]+1]!=0): #eins nach rechts!
+		    final[nonzero_val[0,j],nonzero_val[1,j]]=final[nonzero_val[0,j],nonzero_val[1,j]+1]
+		    check = True
+
+		else:
+		    i+=1
+		    final[nonzero_val[0,j],nonzero_val[1,j]]=i
+
+
+	    return final
+
+
+
+
+
+    def object_filter(self,segmented_img,orig_img):
+	#for future: filter has to become adaptive to depth
+	i=np.max(segmented_img)
+	for k in range(1,i+1): #durch alle zahlen durchiterieren
+		#first only keep large elements then eval their shape
+		if (np.sum((segmented_img == k))<100): #skip all those who were merged away or have not enough pixels tiefenabh???
+		    segmented_img[(segmented_img == k)]=0
+		else:
+		    #DIE BREITE MUSS ICH AUF DER GLEICHEN HOEHE NEHMEN!!!!!!
+		    B=np.copy(segmented_img)
+		    B[(B != k)]=0
+		    C=np.nonzero(B)
+		    ITER=np.reshape(C, (2,-1))
+		    #print C 
+		    top=np.min(C[0])
+		    bottom=np.max(C[0])
+		    left=np.min(C[1])
+		    right=np.max(C[1])
+		    height=bottom-top #da bottom hoehere pixzahl hat!!
+		    total_width=right-left
+
+		    height_left=np.max(ITER[0,(C[1]==left)]) #ACHTUNG:kann mehrere WERTE HABEN
+		    width_height_left = np.max(ITER[1,(C[0]==height_left)])
+		    #WIDTH AT HEIGHT OF LEFT POSITION
+		    width_left= width_height_left-left
+
+		    height_right=np.max(ITER[0,(C[1]==right)]) #ACHTUNG:kann mehrere WERTE HABEN
+		    width_height_right = np.min(ITER[1,(C[0]==height_right)])
+		    #WIDTH AT HEIGHT OF RIGHT POSITION
+		    width_right= right-width_height_right
+
+		    #print width_right
+		    #print width_left
+		    #print width_right
+		    #print height_left
+		    #print height_right
+
+		    #print "NEW OBJECT:"
+		    #print bottom
+		    #print width_right
+		    #print 1.0*width_right/bottom
+
+		    #WIDTH AT TOP
+		    #MUSS NOCH ABFRAGE HIN DAMIT MAN NICHT OUT OF BOUNDS LAEUFT
+		    #width_top = np.max(ITER[1,(C[0]==top+int(0.5*height))])-np.min(ITER[1,(C[0]==top+int(0.5*height))])
+
+		    #if (abs(height_left-height_right)>10): #FILTER 1
+		    #    final[(final == k)]=0
+		    #if (abs(width_top-width_right)<20): #FILTER 2
+		    #    final[(final == k)]=0
+
+		    if (1.0*width_right/bottom<0.25): #FILTER 3
+		        segmented_img[(segmented_img == k)]=0
+
+		    else:
+		        #UEBERGABE?
+		        #obst_arr[0,entry]=int(np.max(C[0])-0.5*width)
+		        #obst_arr[1,entry]=int(np.max(C[1])-0.5*height)
+		        #obst_arr[2,entry]=int(0.5*width)
+		        #obst_arr[3,entry]=int(0.5*height)
+		        #entry+=1
+		        cv2.rectangle(orig_img,(np.min(C[1]),np.min(C[0])),(np.max(C[1]),np.max(C[0])),(0,255,0),3)
+
+	    #eig box np.min breite und hoehe!! if they passed the test!!!!
+	    #print abc
+	    
+	return orig_img
+
+
+
+
+
+
+
+    def ground2pixel(self, point):
+        '''Transforms point in ground coordinates to point in image
+        coordinates using the inverse homography'''
+	point_calc=np.zeros((3,1),dtype=np.float)
+	point_calc= np.dot(self.H,[[point[0]],[point[1]],[1]])
+
+	pixel_int=[int((point_calc[0])/point_calc[2]),int((point_calc[1])/point_calc[2])]
+	#print pixel_float
+        return pixel_int
+
+    def just2pixel(self, point):
+        '''Draw Lines around picture'''
+        return [point[0]*640,point[1]*480]
+
+
+    def render_segments(self, image):
+        for segment in self.map_data["segments"]:	
+            pt_x = []
+            pt_y = []
+            for point in segment["points"]:
+                frame, ground_point = self.map_data["points"][point]
+                pixel = []
+                if frame == 'axle':
+                    pixel = self.ground2pixel(ground_point)
+                elif frame == 'camera':
+                    pixel = ground_point
+                elif frame == 'image01':
+                    pixel = self.just2pixel(ground_point)
+                else:
+                    logger.info('Unkown reference frame. Using "axle" frame')
+                    pixel = self.ground2pixel(ground_point)
+                pt_x.append(pixel[0])
+                pt_y.append(pixel[1])
+            color = segment["color"]
+            image = self.draw_segment(image, pt_x, pt_y, color)
+        return image
+
+    def draw_segment(self, image, pt_x, pt_y, color):
+        defined_colors = {
+            'red' : ['rgb', [1, 0, 0]],
+            'green' : ['rgb', [0, 1, 0]],
+            'blue' : ['rgb', [0, 0, 1]],
+            'yellow' : ['rgb', [1, 1, 0]],
+            'magenta' : ['rgb', [1, 0 ,1]],
+            'cyan' : ['rgb', [0, 1, 1]],
+            'white' : ['rgb', [1, 1, 1]],
+            'black' : ['rgb', [0, 0, 0]]}
+        color_type, [r, g, b] = defined_colors[color]
+        cv2.line(image, (pt_x[0], pt_y[0]),(pt_x[1], pt_y[1]),(b * 255, g* 255, r * 255), 5)
+        return image
---
+++ catkin_ws/src/25-devel-saviors/obst_avoid/src/obstacle_detection_node.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import CompressedImage
+
+### note you need to change the name of the robot to yours here
+from obst_avoid.detector import Detector
+from duckietown_utils import get_base_name, rgb_from_ros
+
+class ObstDetectNode(object):
+    """
+    Obstacle Detection Node
+    """
+    def __init__(self):
+        self.node_name = "Obstacle Detecion Node"
+        robot_name = rospy.get_param("~robot_name", "")
+	self.count = 1
+
+        self.detector = Detector(robot_name=robot_name)
+
+        # Create a Publisher
+        self.pub_topic = '/{}/ObstDetect/image/compressed'.format(robot_name)
+        self.publisher = rospy.Publisher(self.pub_topic, CompressedImage, queue_size=1)
+
+        # Create a Subscriber
+        self.sub_topic = '/{}/camera_node/image/compressed'.format(robot_name)
+        self.subscriber = rospy.Subscriber(self.sub_topic, CompressedImage, self.callback)
+
+    def callback(self, image):
+	if (self.count==10): #only run with 30/self.count Hz
+		#print "HELLO CALLLBACK HERE"
+		obst_image = CompressedImage()
+		obst_image.header.stamp = image.header.stamp
+		obst_image.format = "jpeg"
+
+		# you should write the following function in your class
+		obst_image.data = self.detector.process_image(rgb_from_ros(image))
+
+		#spaeter: anstatt obst_image.data eher
+		#1. EXTRACT OBSTACLES 
+		#2. SEND THEM
+		#3. VISUALIE THEM!
+
+		# publish new message
+		self.publisher.publish(obst_image.data)
+		self.count=1
+	else:
+		self.count+=1
+	
+
+    def onShutdown(self):
+        rospy.loginfo('Shutting down Obstacle Detection, back to unsafe mode')
+
+# MEINER MEINUNG NACH HIER DANN WARSCH 2.NODE AUCH NOCH REIN WO DANN DIE OBST AVOIDANCE GEMACHT WIRD ODER SO
+
+if __name__ == '__main__':
+    rospy.init_node('obst_detection_node', anonymous=False)
+    obst_detection_node = ObstDetectNode()
+    rospy.on_shutdown(obst_detection_node.onShutdown)
+    rospy.spin()
