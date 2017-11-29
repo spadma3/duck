adee308ac1d33c97d6997446acce31516173f65b
SebasRatz <sratz@student.ethz.ch>
2017-11-29 10:39:46 +0100

    refactoring navigation

--- catkin_ws/src/20-indefinite-navigation/navigation/setup.py
+++ catkin_ws/src/20-indefinite-navigation/navigation/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['fleet_planning','rqt_navigation'],
+    packages=['navigation','rqt_navigation'],
     package_dir={'': 'include'},
     requires=['std_msgs', 'rospy']
 )
