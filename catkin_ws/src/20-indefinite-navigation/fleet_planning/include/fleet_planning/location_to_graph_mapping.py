887f02a97b3b8b433d7cd5fc0a32c2afb70d5ffc
SebasRatz <sratz@student.ethz.ch>
2017-12-03 12:22:23 +0100

    finished setup of test node. working. next: test at eth

--- catkin_ws/src/20-indefinite-navigation/fleet_planning/include/fleet_planning/location_to_graph_mapping.py
+++ catkin_ws/src/20-indefinite-navigation/fleet_planning/include/fleet_planning/location_to_graph_mapping.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 class MappingTile:
-    tile_length = 200  # TODO: get this number
+    tile_length = 0.595  # TODO: retrieve this from parameter file in duckietown/00-infrastructure/config/baseline/duckeitown_description
     _tile = None
 
     def __init__(self, tile):
@@ -9,24 +9,34 @@ class MappingTile:
 
     @property
     def tile_center_location(self):
-        return [self._tile.x * self.tile_length + self.tile_length / 2.0, self._tile.y * self.tile_length + self.tile_length / 2.0]
+        return np.array([self._tile.x * self.tile_length + self.tile_length / 2.0, self._tile.y * self.tile_length + self.tile_length / 2.0])
 
     def distance_to(self, position_2d):
         return np.sqrt(np.sum(np.square(self.tile_center_location - position_2d)))
 
     def node_from_duckiebot_rotation(self, db_rotation):
+        db_rotation = np.around(db_rotation, -1)
+
+        rest = db_rotation % 90
+        if rest > 45:
+            db_rotation += 90 - rest
+        else:
+            db_rotation -= rest
+
         diff_rotation = (db_rotation - self._tile.rotation) % 360
 
         # following the convention of how nodes are numbered at intersections, this holds. Yes, not very good design.
-        if diff_rotation == 0:
-            return self._tile.node5.name
-        elif diff_rotation == 90:
-            return self._tile.node3.name
-        elif diff_rotation == 180:
-            return self._tile.node1.name
-        elif diff_rotation == 270:
-            return self._tile.node7.name
-
+        try:
+            if diff_rotation == 0:
+                return self._tile.node5.name
+            elif diff_rotation == 90:
+                return self._tile.node3.name
+            elif diff_rotation == 180:
+                return self._tile.node1.name
+            elif diff_rotation == 270:
+                return self._tile.node7.name
+        except AttributeError:
+            rospy.logwarn('Robot was located at illegal part of the intersection, localization ignored.')
 
 class IntersectionMapper:
     mapping_tiles = []
@@ -37,17 +47,19 @@ class IntersectionMapper:
             if 'way' in tile.type:
                 self.mapping_tiles.append(MappingTile(tile))
 
-    def get_node_name(self, position_2d, orientation):
+    def get_node_name(self, position_2d_m, rotation_deg):
         """
         given a position and a rotation of a duckiebot that is at a stop line (not checked),
         it returns the corresponding node on the graph.
-        :param position_2d: [x,y]
-        :param orientation: rotation on map, a float
+        :param position_2d_m: [x,y] in meters
+        :param rotation_deg: z rotation in degrees on map, a float
         :return: node number as int
         """
         for intersection in self.mapping_tiles:
-            if intersection.distance_to(position_2d) < self.max_radius * intersection.tile_length:
-                return intersection.node_from_duckiebot_rotation(orientation)
+            if intersection.distance_to(position_2d_m) < self.max_radius * intersection.tile_length:
+                return intersection.node_from_duckiebot_rotation(rotation_deg)
+            else:
+                rospy.logwarn('Robot was located too far from intersection. Localization ignored.')
 
         return None
 
@@ -59,10 +71,10 @@ if __name__ == '__main__':
     from generate_duckietown_map import graph_creator
     import tf
     import rospy
-    print('AHAAAA')
+    import tf2_ros
+
     rospy.init_node('location_to_graph_mapping')
     listener = tf.TransformListener()
-
     # load csv
     script_dir = os.path.dirname(__file__)
     csv_filename = 'tiles_lab'
@@ -72,15 +84,23 @@ if __name__ == '__main__':
     gc.build_graph_from_csv(map_path, csv_filename)
     rospy.loginfo('started!!')
     mapper = IntersectionMapper(gc)
-    while True:
-        rospy.sleep(5.0)
-        #listener.waitForTransform('duckiebot', 'world', rospy.Time(), rospy.Duration(4.0))
-        try:
-            (trans, rot) = listener.lookupTransform('duckiebot', 'world', rospy.Time(0))
-        except:
-            pass
-        node = mapper.get_node_name(np.array([500, 800]).reshape(-1, 1), 180)
-        rospy.loginfo(node)
 
-    rospy.on_shutdown(actions_dispatcher_node.onShutdown)
-    rospy.spin()
+    rate = rospy.Rate(5.0)
+    while not rospy.is_shutdown():
+        try:
+            listener.waitForTransform('world', 'duckiebot', rospy.Time(), rospy.Duration(4.0))
+        except tf2_ros.TransformException:
+            rospy.logwarn('transform did not appear!')
+
+        try:
+            (trans, rot) = listener.lookupTransform('world', 'duckiebot', rospy.Time(0))
+        except tf2_ros.LookupException:
+            rospy.logwarn('transform could not be found')
+
+        rot = tf.transformations.euler_from_quaternion(rot)[2]
+
+        node = mapper.get_node_name(trans[:2], np.degrees(rot))
+
+        rate.sleep()
+
+
