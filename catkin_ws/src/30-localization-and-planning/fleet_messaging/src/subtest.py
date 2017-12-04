aee2b4143e8c3a4cf072f69d56cc009a89c28543
somml <somml@student.ethz.ch>
2017-12-04 23:02:44 +0100

    commlibs really working now

--- catkin_ws/src/30-localization-and-planning/fleet_messaging/src/subtest.py
+++ catkin_ws/src/30-localization-and-planning/fleet_messaging/src/subtest.py
@@ -1,26 +1,23 @@
 # Testing
 import commlibs
 import commlibs2
-import multiprocessing
+import thread
 
 sub = []
 
-sub.append(commlibs.duckie0mq(type = 'sub', port = "5555"))
-sub.append(commlibs2.duckie0mq(interface = "wlp1s0", type = 'sub'))
+sub1 = commlibs.duckie0mq(type = 'sub', port = "5555")
+sub2 = commlibs2.duckie0mq(interface = "wlp1s0", type = 'sub')
 
-print(sub[0].rcv_string())
-print(sub[1].rcv_string())
-
-print(sub[0].rcv_string())
-print(sub[1].rcv_string())
-
-def worker(sub,i):
+def recieve(sub):
     while True:
-        print("Process" + str(i) + ": " + sub[i].rcv_string())
+        print(sub.rcv_string())
     return
 
-jobs = []
-for i in range(2):
-    p = multiprocessing.Process(target=worker, args=(sub,i))
-    jobs.append(p)
-    p.start()
+try:
+    thread.start_new_thread(recieve, (sub1,))
+    thread.start_new_thread(recieve, (sub2,))
+except:
+    print("Error on spawning threads")
+
+while True:
+    pass
