42a7c0c3f1a901e3d08da4b6c750f39d9968a9b1
Unknown <nicoscheidt@gmx.de>
2017-12-05 19:10:22 +0100

    Adds demo of new UI (cannot send requests from map yet)

--- catkin_ws/src/20-indefinite-navigation/fleet_planning/include/rqt_fleet_planning/rqt_fleet_planning.py
+++ catkin_ws/src/20-indefinite-navigation/fleet_planning/include/rqt_fleet_planning/rqt_fleet_planning.py
@@ -2,8 +2,10 @@ import os, sys, pickle, rospy
 
 from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi
+from PyQt5 import QtCore, QtGui
 from python_qt_binding.QtWidgets import QWidget
 from duckietown_msgs.msg import SourceTargetNodes
+from fleet_planning.transformation import Transformer
 #path_dir = os.path.dirname(__file__) + '/../../scripts/'
 #sys.path.append(path_dir)
 from fleet_planning.generate_duckietown_map import graph_creator
@@ -20,12 +22,12 @@ class RQTFleetPlanning(Plugin):
         ui_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'rqt_fleet_planning.ui')
         loadUi(ui_file, self._widget)
         self._widget.setObjectName('rqt_fleet_planning')
-        # Show _widget.windowTitle on left-top of each plugin (when 
-        # it's set in _widget). This is useful when you open multiple 
-        # plugins at once. Also if you open multiple instances of your 
-        # plugin at once, these lines add number to make it easy to 
-        # tell from pane to pane.
-        
+
+        #Load parameters
+        self.map_name = rospy.get_param('/map_name', 'tiles_lab')
+        self.script_dir = os.path.dirname(__file__)
+        self.super_script_dir = self.script_dir + '/../../src/'
+
         if context.serial_number() > 1:
             self._widget.setWindowTitle(self._widget.windowTitle() + (' (%d)' % context.serial_number()))
         # Add widget to the user interface
@@ -38,14 +40,26 @@ class RQTFleetPlanning(Plugin):
         self.pub = rospy.Publisher(self.topic_name,SourceTargetNodes, queue_size=1, latch=True)
         self._widget.buttonFindPlan.clicked.connect(self.requestPlan)
 
+        #loading a map image
+        image_path = os.path.abspath(self.super_script_dir + '/maps/' + self.map_name + '_map.png')
+        image = QtGui.QPixmap(image_path)
+        self._widget.label_image.setGeometry(QtCore.QRect(10, 10, image.width(), image.height())) #(x, y, width, height)
+        self._widget.label_image.setPixmap(image)
+        self._widget.label_image.mousePressEvent = self.getPos
+        #todo: remove hardcoding
+        self.transformer = Transformer(101,6)
+
+    def getPos(self , event):
+        self._widget.label_x.setText("Pixel Position x: " + str(event.pos().x()))
+        self._widget.label_y.setText("Pixel Position y: " + str(event.pos().y()))
+        tile_position = self.transformer.image_to_map((event.pos().x(),event.pos().y()))
+        self._widget.label_x_tile.setText("Tile Position x: " + str(tile_position[0]))
+        self._widget.label_y_tile.setText("Tile Position y: " + str(tile_position[1]))
 
     def loadComboBoxItems(self):
         # Loading map
-        self.map_name = rospy.get_param('/map_name', 'tiles_lab')
-        self.script_dir = os.path.dirname(__file__)
-        super_script_dir = self.script_dir + '/../../src/'
         gc = graph_creator()
-        gc.build_graph_from_csv(script_dir=super_script_dir, csv_filename=self.map_name)
+        gc.build_graph_from_csv(script_dir=self.super_script_dir, csv_filename=self.map_name)
 
         node_locations = gc.node_locations
         #comboBoxList = sorted([int(key) for key in node_locations if key[0:4]!='turn'])
