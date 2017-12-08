f411caad575e8e778cc197fd8e672199474c8ba9
manuelbre <bmanuel@ethz.ch>
2017-12-08 17:11:51 +0100

    update

--- catkin_ws/src/10-lane-control/lane_control/scripts/lane_controller_node.py
+++ catkin_ws/src/10-lane-control/lane_control/scripts/lane_controller_node.py
@@ -153,7 +153,7 @@ class lane_controller(object):
         self.stop_line_reading = stopline_msg
         x0=0.12
         rospy.loginfo(str(stopline_msg))
-        if stopline_msg.stop_line_point.x<0.5:
+        if stopline_msg.stop_line_point.x<0.7:
             self.v_bar = self.setupParameter("~v_bar",(stopline_msg.stop_line_point.x-x0)*self.v_bar)
 
         # rospy.loginfo(stopline_msg.stop_line_point.x)
