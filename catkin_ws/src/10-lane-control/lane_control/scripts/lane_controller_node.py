4e2781c8aecb8645f4a159af5c4fc7b800c16b47
manuelbre <bmanuel@ethz.ch>
2017-12-08 16:59:07 +0100

    fix

--- catkin_ws/src/10-lane-control/lane_control/scripts/lane_controller_node.py
+++ catkin_ws/src/10-lane-control/lane_control/scripts/lane_controller_node.py
@@ -152,7 +152,7 @@ class lane_controller(object):
     def deacceleration(self,stopline_msg):
         self.stop_line_reading = stopline_msg
         x0=0.12
-        rospy.loginfo(stopline_msg)
+        rospy.loginfo(str(stopline_msg))
         if stopline_msg.stop_line_point.x<0.5:
             self.v_bar = self.setupParameter("~v_bar",(stopline_msg.stop_line_point.x-x0)*self.v_bar)
 
