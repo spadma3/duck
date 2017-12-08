732ea8000d1ba2733ee6a983a62d28c90b85b506
manuelbre <bmanuel@ethz.ch>
2017-12-08 13:57:23 +0100

    test

--- catkin_ws/src/10-lane-control/lane_control/scripts/lane_controller_node.py
+++ catkin_ws/src/10-lane-control/lane_control/scripts/lane_controller_node.py
@@ -121,11 +121,11 @@ class lane_controller(object):
         car_control_msg = Twist2DStamped()
         car_control_msg.header = lane_pose_msg.header
 
-        # if ~isempty(self.stop_line_reading):
-        #     x_0=0.012 # Set Distance in metre before stop line where we want to stop.
-        # By definition the distance from the center of the red line to the point A should be 0.10 to 0.16 metre
-        #     v_deacc=(stop_line_reading.stop_line_point.x-x0)*self.v_bar
-        #     self.v_bar = self.setupParameter("~v_bar",v_deacc)
+        if self.stop_line_reading:
+            x_0=0.012 # Set Distance in metre before stop line where we want to stop.
+                        # By definition the distance from the center of the red line to the point A should be 0.10 to 0.16 metre
+            v_deacc=(stop_line_reading.stop_line_point.x-x0)*self.v_bar
+            self.v_bar = self.setupParameter("~v_bar",v_deacc)
 
         car_control_msg.v = self.v_bar #*self.speed_gain #Left stick V-axis. Up is positive
 
@@ -152,9 +152,9 @@ class lane_controller(object):
     def deacceleration(self,stopline_msg):
         self.stop_line_reading = stopline_msg
 
-        rospy.loginfo(stopline_msg.stop_line_point.x)
-        rospy.loginfo(stopline_msg.stop_line_point.y)
-        rospy.loginfo(stopline_msg.stop_line_point.z)
+        # rospy.loginfo(stopline_msg.stop_line_point.x)
+        # rospy.loginfo(stopline_msg.stop_line_point.y)
+        # rospy.loginfo(stopline_msg.stop_line_point.z)
 
 
 if __name__ == "__main__":
