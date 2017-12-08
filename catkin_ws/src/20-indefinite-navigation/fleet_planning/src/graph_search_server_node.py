1d727fac550a494e6050481561876bc00418c00a
Benjamin Hahn <hahnb@student.ethz.ch>
2017-12-08 17:13:56 +0100

    small change

--- catkin_ws/src/20-indefinite-navigation/fleet_planning/src/graph_search_server_node.py
+++ catkin_ws/src/20-indefinite-navigation/fleet_planning/src/graph_search_server_node.py
@@ -9,7 +9,7 @@ from cv_bridge import CvBridge
 from fleet_planning.srv import *
 from fleet_planning.generate_duckietown_map import graph_creator, MapImageCreator
 from fleet_planning.transformation import Transformer
-from location_to_graph_mapping import IntersectionMapper
+from fleet_planning.location_to_graph_mapping import IntersectionMapper
 
 class graph_search_server():
     def __init__(self):
