7583c92e4a9d3c2fd7edca254d155fefc36ecd30
Benjamin Hahn <hahnb@student.ethz.ch>
2017-12-09 15:48:04 +0100

    small change

--- catkin_ws/src/20-indefinite-navigation/fleet_planning/include/fleet_planning/transformation.py
+++ catkin_ws/src/20-indefinite-navigation/fleet_planning/include/fleet_planning/transformation.py
@@ -5,8 +5,8 @@ class Transformer():
 
     # this maps a zero-based tile location to a tile-midpoint in pixel coordinates
     def map_to_image(self, point_on_map):
-        return (int((point_on_map[0] + 0.5 )* self.tile_size),
-                int((self.tiles_y - point_on_map[1] - 0.5) * self.tile_size))
+        return (int((point_on_map[0])* self.tile_size),
+                int((self.tiles_y - point_on_map[1]) * self.tile_size))
 
     # this maps a pixel point to the zero-based tile location the pixel is in
     def image_to_map(self, point_on_image):
