9a082c1f1997c4ef5402aa1377ea69b6e5e0103c
NilsFunk <funkn@student.ethz.ch>
2017-12-10 21:23:31 +0100

    new subscription

--- catkin_ws/src/parking/src/image_undistorter.cpp
+++ catkin_ws/src/parking/src/image_undistorter.cpp
@@ -9,7 +9,7 @@ ImageUndistorter::ImageUndistorter()
         : it_(nh_)
 {
     distorted_image_sub_ = it_.subscribe("/schumi/camera_node/image", 1, &ImageUndistorter::imageCallback, this);
-    undistorted_image_pub_ = it_.advertise("/schumi/undistorted_image", 1);
+    undistorted_image_pub_ = it_.advertise("/schumi/camera_node/undistorted_image", 1);
 
     intrinsic_ = (cv::Mat_<double>(3,3)<<353.32880109224254, 0.0, 340.6187776422325, 0.0, 350.03231511108174, 256.42403699758046, 0.0, 0.0, 1.0);
     distCoeffs_ = (cv::Mat_<double>(5,1) <<  -0.3120820098153013, 0.07611370059003515, -0.0043552007678532715, -0.00045294362665816284, 0.0);
@@ -22,13 +22,10 @@ ImageUndistorter::~ImageUndistorter()
 
 void ImageUndistorter::imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
-    cv_bridge::CvImagePtr cv_ptr_distorted;
     cv_bridge::CvImagePtr cv_ptr_undistorted;
-
 
     try
     {
-        cv_ptr_distorted = cv_bridge::toCvCopy(msg);
         cv_ptr_undistorted = cv_bridge::toCvCopy(msg);
 
     }
@@ -38,10 +35,13 @@ void ImageUndistorter::imageCallback(const sensor_msgs::ImageConstPtr& msg)
         return;
     }
 
-    cv::Mat distorted_img = cv_ptr_distorted->image;
     cv::Mat undistorted_img = cv_ptr_undistorted->image;
+    cv::Mat distorted_img = undistorted_img.clone();
 
     cv::undistort(distorted_img, undistorted_img, intrinsic_, distCoeffs_);
+
+    std::cout << undistorted_img << std::endl;
+
     undistorted_image_pub_.publish(cv_ptr_undistorted->toImageMsg());
 }
 
