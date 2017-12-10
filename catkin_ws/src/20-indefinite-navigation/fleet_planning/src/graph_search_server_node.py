01eb0458a2fd9c641c1e413cb3ecc860f9ad4081
Benjamin Hahn <hahnb@student.ethz.ch>
2017-12-10 12:58:33 +0100

    small change

--- catkin_ws/src/20-indefinite-navigation/fleet_planning/src/graph_search_server_node.py
+++ catkin_ws/src/20-indefinite-navigation/fleet_planning/src/graph_search_server_node.py
@@ -122,53 +122,7 @@ class graph_search_server():
         y_start = point[0]  
         y_end = y_start + icon.shape[1]
         map_image[x_start:x_end, y_start:y_end, :] = icon
-        # for trip in trips:
-        #     print "drawing trip's icons...", trip
 
-        #     # draw start location of duckiebot
-        #     start_location = tf.map_to_image(trip[0])  # TODO: verify this function does what it should
-        #     print "start location: ", start_location
-        #     x_start = start_location[0]
-        #     x_end = x_start + self.start_icon.shape[0]
-        #     y_start = start_location[1]  
-        #     y_end = y_start + self.start_icon.shape[1]
-        #     map_image[x_start:x_end, y_start:y_end, :] = self.start_icon
-
-        #     customer_location = tf.map_to_image(trip[1])
-        #     print "customer location: ", customer_location
-        #     x_start = customer_location[0]
-        #     x_end = x_start + self.customer_icon.shape[0]
-        #     y_start = customer_location[1]  
-        #     y_end = y_start + self.customer_icon.shape[1]
-        #     map_image[x_start:x_end, y_start:y_end, :] = self.customer_icon
-
-        #     target_location = tf.map_to_image(trip[2])
-        #     print "target location: ", target_location
-        #     x_start = target_location[0]
-        #     x_end = x_start + self.target_icon.shape[0]
-        #     y_start = target_location[1]  
-        #     y_end = y_start + self.target_icon.shape[1]
-        #     map_image[x_start:x_end, y_start:y_end, :] = self.target_icon
-        # print "displayed all trips"
-            # self.icon_image = 
-        # for node in self.duckietown_graph._nodes:
-        #     node_location = np.round(self.duckietown_graph.get_node_pos(node) * 80)  # tile_length = 80 pixels
-        #     print(node, node_location)
-        #     self.icon_image[node_location[0], node_location[1], 0] = 255  # R
-        #     self.icon_image[node_location[0], node_location[1], 1] = 0  # G
-        #     self.icon_image[node_location[0], node_location[1], 2] = 0  # B
-        # self.duckietown_graph.graph.get_node_posi
-         # mask = cv2.cvtColor(self.icon_image, cv2.COLOR_BGR2GRAY)
-        # ret, mask = cv2.threshold(mask, 10, 255, cv2.THRESH_BINARY)
-        # mask_inv = cv2.bitwise_not(mask)
-        # final_image_bg = cv2.bitwise_and(overlay, overlay, mask = mask_inv)
-        # final_image_fg = cv2.bitwise_and(self.icon_image, self.icon_image, mask = mask)
-        # final_image = cv2.add(final_image_bg, final_image_fg)
-
-        # add all customer logos where desired
-        
-
-        # overlay = cv2.addWeighted(self.icon_image, 1, overlay, 0.5, 0)
         return map_image
 
     def publishImage(self, req, path):
@@ -185,7 +139,8 @@ class graph_search_server():
         print "req: ", req.source_node, req.target_node
         # draw request if initialized, i.e. nonzero
         if req.target_node != '0':
-            overlay = self.draw_icons(overlay, "customer", location = req.target_node) #trips = [[[0, 1], [2, 1], [3, 3]], [[0, 0], [1, 1], [2.5, 2.5]]])
+            overlay = self.draw_icons(overlay, "start", location = req.source_node)
+            overlay = self.draw_icons(overlay, "target", location = req.target_node) 
         self.image_pub.publish(self.bridge.cv2_to_imgmsg(overlay, "bgr8"))
 
     def prepImage(self):
