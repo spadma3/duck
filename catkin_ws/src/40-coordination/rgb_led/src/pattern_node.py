60d0e492742e0b23e92a6462466028e5ab40e209
Sandro Meier <sanmeier@student.ethz.ch>
2017-12-14 20:51:48 +0100

    Fix import errors

--- catkin_ws/src/40-coordination/rgb_led/src/pattern_node.py
+++ catkin_ws/src/40-coordination/rgb_led/src/pattern_node.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import rospy
-from rgb_led import RGB_LED
+from rgb_led import RGB_LED, LED, COLORS
 from rgb_led.srv import PlayLEDPattern
 from rgb_led.duckietown_lights import DuckietownLights
 
