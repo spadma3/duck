eb22945d8debf45208fc4f3efd623e995b7b31a9
Sandro Meier <sanmeier@student.ethz.ch>
2017-12-14 22:10:12 +0100

    Add fancy2 to patterns

--- catkin_ws/src/40-coordination/rgb_led/include/rgb_led/duckietown_lights.py
+++ catkin_ws/src/40-coordination/rgb_led/include/rgb_led/duckietown_lights.py
@@ -8,6 +8,7 @@ from patterns.blinking1 import Blinking1
 from patterns.blinking2 import Blinking2
 from patterns.blinking3 import Blinking3
 from patterns.fancy1 import Fancy1
+from patterns.fancy2 import Fancy2
 
 
 class DuckietownLights():
@@ -113,6 +114,7 @@ DuckietownLights.add_pattern(Blinking1())
 DuckietownLights.add_pattern(Blinking2())
 DuckietownLights.add_pattern(Blinking3())
 DuckietownLights.add_pattern(Fancy1())
+DuckietownLights.add_pattern(Fancy2())
 
 # Enable this to create all kinds of single color blinking patterns.
 #create_color_patterns()
