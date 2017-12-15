060d814ff0929640424868e37cdcb64e5574c3e3
AnnaDaiZH <adai@student.ethz.ch>
2017-12-15 20:21:34 +0100

    change number of ranges

--- catkin_ws/src/10-lane-control/lane_filter/src/lane_filter_node.py
+++ catkin_ws/src/10-lane-control/lane_filter/src/lane_filter_node.py
@@ -80,8 +80,8 @@ class LaneFilterNode(object):
         #sum_phi_l=np.sum(phi_max[1:3])
         #sum_d_l =np.sum(d_max[1:3])
         #av_phi_l=np.average(phi_max[1:3])
-        av_d_l =np.average(d_max[1:3])
-        print "av_dl", av_d_l
+        av_d_l =np.mean(d_max[1:3])
+        print "mean_dl", av_d_l
         me_phi_l=np.median(phi_max[1:3])
         me_d_l =np.median(d_max[1:3])
         print "median phi d ", me_phi_l , me_d_l
