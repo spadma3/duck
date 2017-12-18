6232557c42d3d349c7b89af8553dbbced88061eb
samuelnyffenegger <samuel.nyffenegger@gmail.com>
2017-12-18 16:48:23 +0100

    parking box added for dubins path

--- catkin_ws/src/50-misc-additional-functionality/parking/path_planning/PythonRobotics/PathPlanning/RRTStarCar/dubins_path_planning.py
+++ catkin_ws/src/50-misc-additional-functionality/parking/path_planning/PythonRobotics/PathPlanning/RRTStarCar/dubins_path_planning.py
@@ -378,25 +378,27 @@ def plot_arrow(x, y, yaw, length=0.1, width=0.06, fc="k", ec="k"):
 
 
 if __name__ == '__main__':
-
-
     print("Dubins path planner sample start!!")
     import matplotlib.pyplot as plt
+    import matplotlib.patches as patches
 
-    start_x = 0.0  # [m]
-    start_y = 0.0  # [m]
-    start_yaw = math.radians(0.0)  # [rad]
+    parking_space = 1
+    start_x = (parking_space-1.0)/4.0+1.0/8.0  # [m]
+    start_y = 0.5  # [m]
+    start_yaw = math.radians(-90)  # [rad]
 
-    end_x = 0.3  # [m]
-    end_y = -0.3  # [m]
-    end_yaw = math.radians(0.0)  # [rad]
+    end_x = 0.2  # [m]
+    end_y = 1.0  # [m]
+    end_yaw = math.radians(90.0)  # [rad]
 
-    curvature = 0.25
+    curvature = 0.15
 
     allow_backwards_on_circle = False
 
     px, py, pyaw, mode, clen = dubins_path_planning(start_x, start_y, start_yaw,
                                                     end_x, end_y, end_yaw, curvature, allow_backwards_on_circle)
+
+    fig, ax = plt.subplots(1)
 
     plt.plot(px, py, label="final course " + "".join(mode))
 
@@ -407,7 +409,12 @@ if __name__ == '__main__':
 #    for (ix, iy, iyaw) in zip(px, py, pyaw):
 #        plot_arrow(ix, iy, iyaw, fc="b")
 
+    # patches.Rectangle((0.0,0.0),1.0,1.0,fill=False)
+    ax.add_patch( patches.Rectangle( (0.0, 0.0), 1.0, 1.0, fill=False ))
     plt.legend()
     plt.grid(True)
     plt.axis("equal")
+    plt.xlim([-0.5,1.5])
+    plt.ylim([-0.5,1.5])
+
     plt.show()
