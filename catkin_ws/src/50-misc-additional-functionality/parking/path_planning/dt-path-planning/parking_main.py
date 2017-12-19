bc70c28374e26a746def3f098e44c9df03e74e8b
samuelnyffenegger <samuel.nyffenegger@gmail.com>
2017-12-19 09:23:30 +0100

    coordinate frame bug fixed

--- catkin_ws/src/50-misc-additional-functionality/parking/path_planning/dt-path-planning/parking_main.py
+++ catkin_ws/src/50-misc-additional-functionality/parking/path_planning/dt-path-planning/parking_main.py
@@ -33,7 +33,7 @@ lanes_length = 310                  # mm at entrance, exit
 
 # path planning parameters
 primitive_backwards_start = True    # drive backwards and plan afterwards
-curvature = 60                      # mm minimal turning radius
+curvature = 200 #60                      # mm minimal turning radius
 allow_backwards_on_circle = False   # use this later together with reeds sheep
 length_red_line = int( (lot_width/2.0 - 2.0*wide_tape_width - 1.0*narrow_tape_width) / 2.0 )
 
@@ -46,21 +46,21 @@ Functions
 # pose assigenment: entrance, parking space, exit
 def pose_from_key(key):
     if key == "entrance" or key == 0:
-        return np.array([lanes_length/2.0, wide_tape_width+length_red_line/2.0, -pi/2.0])
+        return np.array([wide_tape_width+length_red_line/2.0, lot_height-lanes_length/2.0, -pi/2.0])
     elif key == "space 1" or key == 1:
-        return np.array([lot_height-space_length/2.0, lot_width/8.0, -pi/2.0])
+        return np.array([lot_width/8.0, space_length/2.0, -pi/2.0])
     elif key == "space 2" or key == 2:
-        return np.array([lot_height-space_length/2.0, lot_width/8.0 + lot_width/4.0, -pi/2.0])
+        return np.array([lot_width/8.0 + lot_width/4.0, space_length/2.0, -pi/2.0])
     elif key == "space 3" or key == 3:
-        return np.array([lot_height-space_length/2.0, lot_width/8.0 + 2*lot_width/4.0, -pi/2.0])
+        return np.array([lot_width/8.0 + 2*lot_width/4.0, space_length/2.0, -pi/2.0])
     elif key == "space 4" or key == 4:
-        return np.array([lot_height-space_length/2.0, lot_width/8.0 + 3*lot_width/4.0, -pi/2.0])
+        return np.array([lot_width/8.0 + 3*lot_width/4.0, space_length/2.0, -pi/2.0])
     elif key == "space 5" or key == 5:
-        return np.array([space_length/2.0, lot_width/8.0 + 2*lot_width/4.0, pi/2.0])
+        return np.array([lot_width/8.0 + 2*lot_width/4.0, lot_height-space_length/2.0, pi/2.0])
     elif key == "space 6" or key == 6:
-        return np.array([space_length/2.0, lot_width/8.0 + 3*lot_width/4.0, pi/2.0])
+        return np.array([lot_width/8.0 + 3*lot_width/4.0, lot_height-space_length/2.0, pi/2.0])
     elif key == "exit" or key == 7:
-        return np.array([lanes_length/2.0, wide_tape_width+narrow_tape_width+3.0/2.0*length_red_line, pi/2.0])
+        return np.array([wide_tape_width+narrow_tape_width+3.0/2.0*length_red_line, lot_height-lanes_length/2.0, pi/2.0])
     else:
         print("parking space not found")
         exit(1)
@@ -77,12 +77,13 @@ if __name__ == '__main__':
     """
     # start and endpose
     start_x, start_y, start_yaw = pose_from_key("entrance")
-    end_x, end_y, end_yaw = pose_from_key("space 1")
+    end_x, end_y, end_yaw = pose_from_key("space 3")
     if do_talking:
         print("initial pose: \n\tx = {}\n\ty = {} \n\ttheta = {}".format(
         start_x, start_y, degrees(start_yaw) ))
         print("end pose: \n\tx = {}\n\ty = {} \n\ttheta = {}".format(
         end_x, end_y, degrees(end_yaw) ))
+        print("curvature = {}".format(curvature))
 
 
     """
@@ -97,12 +98,13 @@ if __name__ == '__main__':
     """
     if do_ploting:
         fig, ax = plt.subplots(1)
-        plt.plot(px, py, label="final course " + "".join(mode))
+        plt.plot(px, py)
+        # plt.plot(px, py, label="final course " + "".join(mode))
         dpp.plot_arrow(start_x, start_y, start_yaw, 0.1*lot_width, 0.06*lot_width, fc="r", ec="r")
         dpp.plot_arrow(end_x, end_y, end_yaw, 0.1*lot_width, 0.06*lot_width, fc="g", ec="g")
 
         ax.add_patch( patches.Rectangle( (0.0, 0.0), lot_width, lot_height, fill=False ))
-        plt.legend()
+        # plt.legend()
         plt.grid(True)
         plt.axis("equal")
         plt.xlim([-visual_boundairy,lot_height+visual_boundairy])
