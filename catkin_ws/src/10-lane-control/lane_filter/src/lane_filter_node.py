53e621efd68fadeff5132d0c645aa148edc090e0
AnnaDaiZH <adai@student.ethz.ch>
2017-12-20 23:33:50 +0100

    outlier rejection for estimation

--- catkin_ws/src/10-lane-control/lane_filter/src/lane_filter_node.py
+++ catkin_ws/src/10-lane-control/lane_filter/src/lane_filter_node.py
@@ -22,6 +22,8 @@ class LaneFilterNode(object):
         self.velocity = Twist2DStamped()
         self.d_median = []
         self.phi_median = []
+        self.store_d =[]
+        self.store_phi =0
         
         # Subscribers
         self.sub = rospy.Subscriber("~segment_list", SegmentList, self.processSegments, queue_size=1)
@@ -73,31 +75,39 @@ class LaneFilterNode(object):
 
         # Step 3: build messages and publish things
         [d_max,phi_max, curvature] = self.filter.getEstimate()
-        #print "d_max = ", d_max
-        #print "phi_max = ", phi_max
-        # sum_phi_l = np.sum(phi_max[1:self.filter.num_belief])
-        # sum_d_l   = np.sum(d_max[1:self.filter.num_belief])
-        # av_phi_l  = np.average(phi_max[1:self.filter.num_belief])
-        # av_d_l    = np.average(d_max[1:self.filter.num_belief])
 
 
         max_val = self.filter.getMax()
         in_lane = max_val > self.filter.min_max
 
-        # delta_dmax = np.median(d_max[1:]) # - d_max[0]
-        # delta_phimax = np.median(phi_max[1:]) #- phi_max[0]
+        # check if the current estimation is an outlier compare to the last 3 estimates
+        if len(self.store_d)<3:
+            self.store_d.append(d_max[0])
+            self.store_phi = phi_max[0]
+        else:
+            # if yes take the top one of the array give it to lane pose and store it again into the array. 
+            if abs(np.mean(self.store_d[0:2])-d_max[0])> 0.15 :
+                d =self.store_d[2]
+                phi =self.store_phi
+                self.store_d.pop(0)
+                self.store_d[2]=self.store_d[1]
+            # else take the new d and store it i
+            else:
+                d = d_max[0]
+                phi =phi_max[0]
+                self.store_d.pop(0)
+                self.store_d[2]= d_max[0]
+                self.store_phi = phi_max[0]
+     
 
-        # if len(self.d_median) >= 5:
-        #     self.d_median.pop(0)
-        #     self.phi_median.pop(0)
-        # self.d_median.append(delta_dmax)
-        # self.phi_median.append(delta_phimax)
+
+
 
         # build lane pose message to send
         lanePose = LanePose()
         lanePose.header.stamp = segment_list_msg.header.stamp
-        lanePose.d =d_max[0]
-        lanePose.phi=phi_max[0]
+        lanePose.d =d
+        lanePose.phi=phi
         lanePose.in_lane = in_lane
         lanePose.status = lanePose.NORMAL
         lanePose.curvature= curvature
@@ -127,7 +137,10 @@ class LaneFilterNode(object):
         #     print "straight line"
         #     lanePose.curvature = 0.0
 
-        # publish the belief image
+        
+
+
+        #  publish the belief image
         bridge = CvBridge()
         belief_img = bridge.cv2_to_imgmsg((255*self.filter.beliefArray[0]).astype('uint8'), "mono8")
         belief_img.header.stamp = segment_list_msg.header.stamp
@@ -136,7 +149,7 @@ class LaneFilterNode(object):
         self.pub_lane_pose.publish(lanePose)
         self.pub_belief_img.publish(belief_img)
 
-        # also publishing a separate Bool for the FSM
+        #  also publishing a separate Bool for the FSM
         in_lane_msg = BoolStamped()
         in_lane_msg.header.stamp = segment_list_msg.header.stamp
         in_lane_msg.data = in_lane
