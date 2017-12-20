c0e6a6ad83996da9e47cb71f80e769cddfae20a9
Kornel Eggerschwiler <kornele@student.ethz.ch>
2017-12-20 13:50:48 +0100

    fixed publishing, time triggered

--- catkin_ws/src/85-implicit-coordination/implicit-coordination/src/implicit.py
+++ catkin_ws/src/85-implicit-coordination/implicit-coordination/src/implicit.py
@@ -3,7 +3,7 @@ import rospy
 import time
 from random import randrange
 from duckietown_msgs.msg import BoolStamped
-from geometry_msgs.msg import PoseStamped
+from duckietown_msgs.msg import VehiclePose
 
 
 class Implicit(object):
@@ -11,17 +11,17 @@ class Implicit(object):
         # Save the name of the node
         self.node_name = rospy.get_name()
         self.bStopline = False
-        self.Pose = []
         self.iteration = 0
-        rospy.loginfo("[%s] Initialzing." % (self.node_name))
+        self.Pose = [0.0,0.0]
 
         # Setup publishers
         self.pub_implicit_coordination = rospy.Publisher("~flag_intersection_wait_go_implicit", BoolStamped, queue_size=1)
         # Setup subscriber
         self.sub_at_intersection = rospy.Subscriber("~flag_at_intersection", BoolStamped, self.cbStop)
-        self.sub_detector = rospy.Subscriber("~vehicle_detection_node", PoseStamped, self.cbPose)
+        self.sub_detector = rospy.Subscriber("~vehicle_detection_node", VehiclePose, self.cbPose)
 
         rospy.loginfo("[%s] Initialzed." % (self.node_name))
+        rospy.Timer(rospy.Duration.from_sec(1.0), self.cbCSMA)
 
     def setupParameter(self, param_name, default_value):
         value = rospy.get_param(param_name, default_value)
@@ -34,29 +34,32 @@ class Implicit(object):
         self.bStopline = msg
 
     def cbPose(self, msg):
-        self.addPose(msg)
-
-    def addPose(self, pose):
-        if len(self.Pose) >= 2:
-            self.Pose.pop(0)
-            self.Pose.append(pose)
+        if msg.detection.data:
+            self.Pose.append(float(msg.rho.data))
+        else:
+            self.Pose.append(self.Pose[0])
+        self.Pose.pop(0)
 
     def DetectMovement(self):
-        if not self.Pose(1) - self.Pose(2) == 0:
+        if not (self.Pose[0] - self.Pose[1] == 0):
             return True
         else:
             return False
 
-    def CSMA(self):
+    def cbCSMA(self, args = None):
+        print 'fuck the duck'
+        flag = BoolStamped()
         SlotTime = 2.0  # in seconds, tunable parameter TODO: experiments
         backoff_time = 0.0  # in seconds
-        if self.DetectMovement:
+        if self.DetectMovement():
             self.iteration += 1
             backoff_time = randrange(0, 2**self.iteration - 1) * SlotTime
+            flag.data = False
             time.sleep(backoff_time)
         else:
             self.iteration = 0
-            self.pub_implicit_coordination
+            flag.data = True
+        self.pub_implicit_coordination.publish(flag)
 
     def on_shutdown(self):
         rospy.loginfo("[%s] Shutting down." % (self.node_name))
@@ -68,8 +71,6 @@ if __name__ == '__main__':
 
     # Create the NodeName object
     node = Implicit()
-
-    node.CSMA()
 
     # Setup proper shutdown behavior
     rospy.on_shutdown(node.on_shutdown)
