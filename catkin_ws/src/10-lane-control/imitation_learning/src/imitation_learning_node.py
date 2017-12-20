bfcb7fea907c3dd08a55241aee06b8c22b0646b9
Rithesh Kumar <ritheshkumar.95@gmail.com>
2017-12-20 12:56:17 -0500

    integrated ncs code

--- catkin_ws/src/10-lane-control/imitation_learning/src/imitation_learning_node.py
+++ catkin_ws/src/10-lane-control/imitation_learning/src/imitation_learning_node.py
@@ -5,6 +5,8 @@ from duckietown_msgs.msg import Twist2DStamped
 from duckietown_utils import rgb_from_ros
 from PIL import Image
 import numpy as np
+from mvnc import mvncapi as mvnc
+from collections import deque
 
 
 class LaneFollowerNode(object):
@@ -23,9 +25,17 @@ class LaneFollowerNode(object):
         # safe shutdown
         rospy.on_shutdown(self.custom_shutdown)
 
-        # timer
-        # self.timer = rospy.Timer(
-        #    rospy.Duration.from_sec(1.0), self.getGains_event)
+        mvnc.SetGlobalOption(mvnc.GlobalOption.LOG_LEVEL, 2)
+        devices = mvnc.EnumerateDevices()
+        if len(devices) == 0:
+            print "No devices found!"
+        self.device = mvnc.Device(devices[0])
+        self.device.OpenDevice()
+        with open('graph', mode='rb') as f:
+            blob = f.read()
+        self.graph = self.device.AllocateGraph(blob)
+        self.image_deque = deque(4)
+
         rospy.loginfo("[%s] Initialized " % (rospy.get_name()))
 
     def custom_shutdown(self):
@@ -46,28 +56,37 @@ class LaneFollowerNode(object):
         img = rgb_from_ros(compressed_image)
         img = Image.fromarray(img)
         img.thumbnail((80, 60))
-        img = np.asarray(img)
+        img = np.asarray(img.convert('L'))
+        self.image_deque.append(img)
 
-        '''
-        Call tensorflow code and get outputs v, omega
-        '''
+        if len(self.image_deque == 4):
+            batch_img = np.asarray(self.image_deque)
+            print batch_img.shape
+            batch_img = batch_img.reshape(4, 60, 80)
+            batch_img = (2*(batch_img/255.) - 1.).astype('float32')
+            batch_img = batch_img.transpose(1, 2, 0)
+
+            out = self.graph.LoadTensor(batch_img.astype(np.float16), 'user object')
+            assert out is True
+            output, userobj = self.graph.GetResult()
+            print "Result: ", output[0]
+        else:
+            output = [0.]
 
         timestamp_now = rospy.Time.now()
         image_delay_stamp = timestamp_now - compressed_image.header.stamp
 
         car_control_msg = Twist2DStamped()
         car_control_msg.header.stamp = image_delay_stamp
-        car_control_msg.v = 10.
-        car_control_msg.omega = 0.5
-
-	rospy.loginfo("Published car control message!")
+        car_control_msg.v = 0.386
+        car_control_msg.omega = output[0]
         self.pub_car_cmd.publish(car_control_msg)
 
     def onShutdown(self):
         rospy.loginfo("[LaneFilterNode] Shutdown.")
 
 if __name__ == '__main__':
-    rospy.init_node('imitation_learner',anonymous=False)
+    rospy.init_node('imitation_learner', anonymous=False)
     lane_follower_node = LaneFollowerNode()
     rospy.on_shutdown(lane_follower_node.onShutdown)
     rospy.spin()
