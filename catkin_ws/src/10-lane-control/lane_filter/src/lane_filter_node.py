113e86899b3891bf5b3b43ffe773359f5bac92bd
AnnaDaiZH <adai@student.ethz.ch>
2017-12-21 15:16:51 +0100

    test outlier rejection

--- catkin_ws/src/10-lane-control/lane_filter/src/lane_filter_node.py
+++ catkin_ws/src/10-lane-control/lane_filter/src/lane_filter_node.py
@@ -88,7 +88,7 @@ class LaneFilterNode(object):
             self.store_phi = phi_max[0]
         else:
             # if yes take the top one of the array give it to lane pose and store it again into the array. 
-            if abs(np.mean(self.store_d[0:2])-d_max[0])> 0.03 :
+            if abs(np.mean(self.store_d[0:2])-d_max[0])> 0.06 :
                 d =self.store_d[2]
                 phi =self.store_phi
                 self.store_d.pop(0)
@@ -102,7 +102,7 @@ class LaneFilterNode(object):
                 self.store_d.append(d_max[0])
                 self.store_phi = phi_max[0]
                 print "accepted"
-            print "length stored d " , len(self.store_d)
+            print "length stored d " , len(self.store_d), self.store_d
      
 
 
