7c2ca92c4b2f11d69840442db33ef8032edfe0b0
AnnaDaiZH <adai@student.ethz.ch>
2017-12-22 14:20:54 +0100

    resolution adaption

--- catkin_ws/src/10-lane-control/lane_filter/src/lane_filter_node.py
+++ catkin_ws/src/10-lane-control/lane_filter/src/lane_filter_node.py
@@ -64,8 +64,8 @@ class LaneFilterNode(object):
 
         # Step 2: update
         range_arr = np.zeros(self.filter.num_belief+1)
-        range_max = 0.8  # range to consider edges in general
-        range_min = 0.0
+        range_max = 0.7  # range to consider edges in general
+        range_min = 0.2
         range_diff = (range_max - range_min)/(self.filter.num_belief - 1)
         
         for i in range(1,self.filter.num_belief + 1):
