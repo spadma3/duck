3d2dbe4d95543d16297facaae76e04e9fa55f52a
Francesco Ferrara <francesco.ferrara@helbling.ch>
2017-12-25 15:01:08 +0100

    Finished implementation of duckiemq.py --> TO BE TESTED

--- catkin_ws/src/30-localization-and-planning/fleet_messaging/include/fleet_messaging/duckiemq.py
+++ catkin_ws/src/30-localization-and-planning/fleet_messaging/include/fleet_messaging/duckiemq.py
@@ -5,7 +5,6 @@ Communication module for Duckiebots
 # Import modules
 from __future__ import print_function
 import time
-import socket
 import netifaces as ni
 import libserialize
 import zmq
@@ -30,7 +29,6 @@ class DuckieMQBase(object):
         self.__context = zmq.Context()
         ownip = ni.ifaddresses(interface)[ni.AF_INET][0]["addr"]
         self.__endpoint = "epgm://" + ownip + ":" + port
-        self.__def_filter = False
         # Uncomment, if needed in future implementations
         # self.__interface = interface
         # self.__ownip = ownip
@@ -77,6 +75,31 @@ class DuckieMQSender(DuckieMQBase):
         # Sleep, to guarantee initialization
         time.sleep(0.2)
 
+    def send_string(self, msg):
+        """
+        Send a string through the socket.
+        Inputs:
+        - msg: string to be sent through the socket
+        Outputs:
+        None
+        Exceptions:
+        - socket.error: if socket type is not "pub"
+        """
+        self.__socket.send_string(msg)
+
+    def send_serialized(self, msg):
+        """
+        Serialize message with libserialize.serialize and send it.
+        Inputs:
+        - msg: Message to be serialized and sent
+        Outputs:
+        None
+        Exceptions:
+        - socket.error: if socket type is not "pub"
+        """
+        self.__socket.send_serialized(msg, libserialize.serialize, flags=0,
+                                      copy=True)
+
 
 class DuckieMQReceiver(DuckieMQBase):
     """
@@ -103,7 +126,7 @@ class DuckieMQReceiver(DuckieMQBase):
         # Configure receiver socket
         self.__socket = self.__context.socket(zmq.SUB)
         self.__socket.setsockopt(zmq.SUBSCRIBE, "")
-        self.__def_filter = False
+        self.__filters = []
         self.__socket.connect(self.__endpoint)
         #self.socket.setsockopt_string(zmq.RATE, self.rate)
         if timeout < 0:
@@ -128,23 +151,22 @@ class DuckieMQReceiver(DuckieMQBase):
         # Call the parent destructor
         super(DuckieMQReceiver, self).__del__()
 
-    def setfilter(self, filter_string):
+    def addfilter(self, filter_string):
         """
-        Set filter to only recieve messages starting with filter_string
+        Add filter to only receive messages starting with filter_string
         (string).
         Inputs:
         - filter_string: string to be filtered
         Outputs:
         None
         """
-        if self.__def_filter:
-            self.removefilter()
-            self.__def_filter = True
         try:
             self.__socket.setsockopt(zmq.SUBSCRIBE, filter_string)
         except TypeError:
             self.__socket.setsockopt_string(zmq.SUBSCRIBE, filter_string)
             print("Set filter: \"" + filter_string + "\" on " + self.__endpoint)
+        finally:
+            self.__filters.append(filter_string)
 
     def removefilter(self, filter_string=""):
         """
@@ -154,169 +176,18 @@ class DuckieMQReceiver(DuckieMQBase):
         - filter_string: string filter to be removed
         Outputs:
         None
+        Exceptions:
+        - ValueError: if filter_string is not in self.__filters
         """
         try:
-            if self.__def_filter:
-                self.__socket.setsockopt(zmq.UNSUBSCRIBE, filter_string)
-                self.__def_filter = False
+            self.__filters.remove(filter_string)
+            self.__socket.setsockopt(zmq.UNSUBSCRIBE, filter_string)
+        except ValueError:
+            raise ValueError("filter_string not set in object.")
         except TypeError:
             self.__socket.setsockopt_string(zmq.UNSUBSCRIBE, filter_string)
             print("Removed filter: \"" + filter_string + "\" on " +
                   self.__endpoint)
-
-
-class DuckieMQ(object):
-    """
-    ZeroMQ class implementation for communication between Duckiebots.
-    """
-    def __init__(self, interface="wlan0", port="5554", socktype="sub",
-                 timeout=0):
-        """
-        Initialzes either a reciever or publisher socket on the specified
-        interface and port.
-        Use ifconfig to determine the name of the correct interface.
-        On standard Duckiebots the interface is called "wlan0".
-        Communication works over epgm protocol (broadcasting)
-        Inputs:
-        - interface: Interface used by the socket
-        - port:      Port used by the socket
-        - socktype:  Socket type ("sub"/"pub")
-        - timeout:   timeout on receiving messages
-        Ouptuts:
-        - object: ZeroMQ socket
-        Exceptions:
-        - ValueError:   if timeout smaller than 0
-        - socket.error: if socket type neither "pub" nor "sub"
-        """
-        self.context = zmq.Context()
-        self.ownip = ni.ifaddresses(interface)[ni.AF_INET][0]["addr"]
-        self.port = port
-        self.def_filter = False
-        endpoint = self.build_endpoint()
-        # Uncomment, if needed in future implementations
-        # self.interface = interface
-
-        # Rate [kbit] can be setup manually (if not, optimal rate is taken)
-        # rate = 40*1000
-
-        # Configure publisher socket
-        if socktype == "pub":
-            self.socket = self.context.socket(zmq.PUB)
-            self.socket.connect(endpoint)  # should connect
-            # self.socket.setsockopt_string(zmq.RATE, rate)
-            print("Publisher initialized on " + endpoint)
-
-        # Configure subscriber socket
-        elif socktype == "sub":
-            self.socket = self.context.socket(zmq.SUB)
-            self.socket.setsockopt(zmq.SUBSCRIBE, "")
-            self.def_filter = True
-            self.socket.connect(endpoint)
-            #self.socket.setsockopt_string(zmq.RATE, rate)
-            if timeout < 0:
-                raise ValueError("timeout must be greater or equal 0!")
-            if timeout > 0:
-                self.poller = zmq.Poller()
-                self.poller.register(self.socket, zmq.POLLIN)
-            print("Subscriber initialized on " + endpoint)
-
-        else:
-            raise socket.error("No known socket type!")
-
-        # Set the socket type and timeout
-        self.socktype = socktype
-        self.timeout = timeout
-
-        # Sleep, to guarantee initialization
-        time.sleep(0.2)
-
-    def cleanup(self):
-        """
-        Destroy the socket.
-        """
-        # Unregister the poller
-        if self.socktype == "sub" and self.timeout > 0:
-            self.poller.unregister(self.socket)
-
-        self.context.destroy()
-
-    def removefilter(self, filter_string=""):
-        """
-        Removes filter_string from filter list, default removes default ""
-        string.
-        Inputs:
-        - filter_string: string filter to be removed
-        Outputs:
-        None
-        Exceptions:
-        - socket.error: if socket type is not "sub"
-        """
-        if self.socktype == "sub":
-            try:
-                self.socket.setsockopt(zmq.UNSUBSCRIBE, filter_string)
-            except TypeError:
-                self.socket.setsockopt_string(zmq.UNSUBSCRIBE, filter_string)
-                print("Removed filter: \"" + filter_string + "\" on " +
-                      self.build_endpoint())
-        else:
-            raise socket.error("Socket not of type subscriber, no filter " +
-                               "changed.")
-
-    def setfilter(self, filter_string):
-        """
-        Set filter to only recieve messages starting with filter_string
-        (string).
-        Inputs:
-        - filter_string: string to be filtered
-        Outputs:
-        None
-        Exceptions:
-        - socket.error: if socket type is not "sub"
-        """
-        if self.socktype == "sub":
-            if self.def_filter:
-                self.removefilter()
-                self.def_filter = False
-            try:
-                self.socket.setsockopt(zmq.SUBSCRIBE, filter_string)
-            except TypeError:
-                self.socket.setsockopt_string(zmq.SUBSCRIBE, filter_string)
-                print("Set filter: \"" + filter_string + "\" on " +
-                      self.build_endpoint())
-        else:
-            raise socket.error("Socket not of type subscriber, no filter " +
-                               "changed.")
-
-    def send_string(self, msg):
-        """
-        Send a string through the socket.
-        Inputs:
-        - msg: string to be sent through the socket
-        Outputs:
-        None
-        Exceptions:
-        - socket.error: if socket type is not "pub"
-        """
-        if self.socktype == "pub":
-            self.socket.send_string(msg)
-        else:
-            raise socket.error("Socket not of type publisher, no message sent.")
-
-    def send_serialized(self, msg):
-        """
-        Serialize message with libserialize.serialize and send it.
-        Inputs:
-        - msg: Message to be serialized and sent
-        Outputs:
-        None
-        Exceptions:
-        - socket.error: if socket type is not "pub"
-        """
-        if self.socktype == "pub":
-            self.socket.send_serialized(msg, libserialize.serialize, flags=0,
-                                        copy=True)
-        else:
-            raise socket.error("Socket not of type publisher, no message sent.")
 
     def rcv_string(self):
         """
@@ -328,14 +199,10 @@ class DuckieMQ(object):
         Exceptions:
         - socket.error: if socket type is not "sub"
         """
-        if self.socktype == "sub":
-            if self.timeout > 0:
-                msg = self.rcv_string_timeout()
-            else:
-                msg = self.socket.recv_string()
+        if self.__timeout > 0:
+            msg = self.rcv_string_timeout()
         else:
-            raise socket.error("Socket not of type subscriber , no message " +
-                               "will be received.")
+            msg = self.__socket.recv_string()
 
         return msg
 
@@ -348,11 +215,11 @@ class DuckieMQ(object):
         msg: Received string (None if timeout occurs, before any msg arrives)
         """
         # Poll for a message
-        events = self.poller.poll(self.timeout)
+        events = self.__poller.poll(self.__timeout)
         if not events:
             msg = None
         else:
-            msg = self.socket.recv_string()
+            msg = self.__socket.recv_string()
 
         return msg
 
@@ -366,15 +233,11 @@ class DuckieMQ(object):
         Exceptions:
         - socket.error: if socket type is not "sub"
         """
-        if self.socktype == "sub":
-            if self.timeout > 0:
-                msg = self.rcv_serialized_timeout()
-            else:
-                msg = self.socket.recv_serialized(libserialize.parse, flags=0,
-                                                  copy=True)
+        if self.__timeout > 0:
+            msg = self.rcv_serialized_timeout()
         else:
-            raise socket.error("Socket not of type subscriber or using " +
-                               "timeout, no message will be received.")
+            msg = self.__socket.recv_serialized(libserialize.parse, flags=0,
+                                                copy=True)
 
         return msg
 
@@ -388,21 +251,11 @@ class DuckieMQ(object):
              arrives)
         """
         # Poll for a message
-        events = self.poller.poll(self.timeout)
+        events = self.__poller.poll(self.__timeout)
         if not events:
             msg = None
         else:
-            msg = self.socket.recv_serialized(libserialize.parse, flags=0,
-                                              copy=True)
+            msg = self.__socket.recv_serialized(libserialize.parse, flags=0,
+                                                copy=True)
 
         return msg
-
-    def build_endpoint(self):
-        """
-        Build an endpoint string.
-        Inputs:
-        None
-        Outputs:
-        endpoint string
-        """
-        return "epgm://" + self.ownip + ":" + self.port
