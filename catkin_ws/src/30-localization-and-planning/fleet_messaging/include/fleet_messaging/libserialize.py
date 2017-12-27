436e3bb5e3c9f3032fdcf7193d155846e6b3df53
Francesco Ferrara <francesco.ferrara@helbling.ch>
2017-12-27 10:24:01 +0100

    - libserialize.py: updated parse function --> TO BE TESTED

--- catkin_ws/src/30-localization-and-planning/fleet_messaging/include/fleet_messaging/libserialize.py
+++ catkin_ws/src/30-localization-and-planning/fleet_messaging/include/fleet_messaging/libserialize.py
@@ -54,7 +54,7 @@ def parse(bma_data):
     bma = ByteMultiArray()
 
     # Parse the data
-    bma_data_joined = str.join(bma_data)
+    bma_data_joined = "".join(bma_data)
     bma.ParseFromString(bma_data_joined)
 
     # Populate the ROS message
