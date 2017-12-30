8383194dd302bef66ecaef674193c912de35e686
Andrea Censi <acensi@idsc.mavt.ethz.ch>
2017-12-30 16:12:45 +0100

    minor

--- catkin_ws/src/00-infrastructure/duckietown/include/duckietown_utils/unit_tests.py
+++ catkin_ws/src/00-infrastructure/duckietown/include/duckietown_utils/unit_tests.py
@@ -1,18 +1,23 @@
 from . import logger
+from .constants import DuckietownConstants
 
 __all__ = [
     'unit_test', 
     'run_tests_for_this_module',
 ]
 
+show_info = DuckietownConstants.debug_show_package_import_info
 
 try: 
     from comptests import comptest as unit_test  # @UnusedImport
     from comptests import run_module_tests as run_tests_for_this_module  # @UnusedImport
-    logger.warning('Using the Comptests testing framework.')
+    
+    if show_info:
+        logger.warning('Using the Comptests testing framework.')
     using_fake_tests = False
 except ImportError:
-    logger.warning('Unit tests are disabled becaused Comptests not found.')
+    if show_info:
+        logger.warning('Unit tests are disabled becaused Comptests not found.')
     using_fake_tests = True
     
 if using_fake_tests:
