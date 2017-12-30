033a4ff36ee329448a06d02a6389448f7f1fed54
Andrea Censi <acensi@ethz.ch>
2017-12-30 22:13:11 +0100

    Small fix

--- catkin_ws/src/00-infrastructure/duckietown/include/duckietown_utils/unit_tests.py
+++ catkin_ws/src/00-infrastructure/duckietown/include/duckietown_utils/unit_tests.py
@@ -4,6 +4,7 @@ from .constants import DuckietownConstants
 __all__ = [
     'unit_test', 
     'run_tests_for_this_module',
+    'get_output_dir_for_test',
 ]
 
 show_info = DuckietownConstants.debug_show_package_import_info
@@ -12,9 +13,10 @@ try:
     from comptests import comptest as unit_test  # @UnusedImport
     from comptests import run_module_tests as run_tests_for_this_module  # @UnusedImport
     from comptests.comptests import get_comptests_output_dir as get_output_dir_for_test  # @UnusedImport
-
+    
     if show_info:
         logger.warning('Using the Comptests testing framework.')
+    
     using_fake_tests = False
 except ImportError:
     if show_info:
