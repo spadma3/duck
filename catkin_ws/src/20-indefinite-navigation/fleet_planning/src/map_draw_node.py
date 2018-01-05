65f8cab6c98e2dabc1a65ca211b976c5d76b0c66
Unknown <nicoscheidt@gmx.de>
2018-01-05 09:05:07 +0100

    trying to find an error

--- catkin_ws/src/20-indefinite-navigation/fleet_planning/src/map_draw_node.py
+++ catkin_ws/src/20-indefinite-navigation/fleet_planning/src/map_draw_node.py
@@ -122,7 +122,6 @@ class MapDrawNode:
         # NOTE: factor -1 added so due to image negative image coordinates in vertical direction.
         height_start = max(self.map_img.shape[0] * -1, point[1] * -1)  
         height_end = min(self.map_img.shape[0], (height_start + icon.shape[0]))
-        print "height end as max: ", self.map_img.shape[0] * -1, (height_start + icon.shape[0])
         width_start =  min(self.map_img.shape[1], point[0] + (icon_number - 1) * (icon.shape[1] + 5))
         width_end = min(self.map_img.shape[1], width_start + icon.shape[1])
         icon = icon[0:height_end - height_start, 0:width_end - width_start]
@@ -166,6 +165,9 @@ class MapDrawNode:
         #todo get updated graph (with path)
         edges_to_draw = None
         if (self._duckie_path_to_draw):
+            print "duckie to draw: ", self._duckie_path_to_draw
+            print "all duckies: "
+            print self.duckiebots.keys()
             node_numbers_as_str = self.duckiebots[self._duckie_path_to_draw].path
             edges_to_draw = []
             for i in range(0,len(node_numbers_as_str)-1):
