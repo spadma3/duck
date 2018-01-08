f0a2266c767a425ddb8d1dffba42ac7fbee0069b
SofianeGadi <sgadi@student.ethz.ch>
2018-01-08 20:20:43 +0100

    Code improvment

--- catkin_ws/src/15-calibration/src/publish_control.py
+++ catkin_ws/src/15-calibration/src/publish_control.py
@@ -5,56 +5,59 @@ from duckietown_msgs.msg import WheelsCmdStamped, Twist2DStamped
 from math import sin, pi
 
 
-# Initialize the node with rospy
-rospy.init_node('command', anonymous=True)
+class calibration:
+	def __init__(self):
+		
+		# Initialize the node with rospy
+                rospy.init_node('command', anonymous=True)
 
-# Create publisher
-publisher=rospy.get_param("~veh")+"/wheels_driver_node/wheels_cmd"
-pub_wheels_cmd = rospy.Publisher(publisher,WheelsCmdStamped,queue_size=1)
+                # Create publisher
+                publisher=rospy.get_param("~veh")+"/wheels_driver_node/wheels_cmd"
+                self.pub_wheels_cmd = rospy.Publisher(publisher,WheelsCmdStamped,queue_size=1)
 
-rospy.loginfo("Calibration starts")
+		self.vFinStraight = rospy.get_param('~vfs')
+		self.stepStraight = rospy.get_param("~ss")
+		self.durationStraight = rospy.get_param("~ds")
+		
 
+	def sendCommand(self, vel_right, vel_left):
+		# Put the wheel commands in a message and publish
+		msg_wheels_cmd = WheelsCmdStamped()
+		msg_wheels_cmd.header.stamp = rospy.Time.now()
+		msg_wheels_cmd.vel_right =vel_right
+		msg_wheels_cmd.vel_left =vel_left
+		self.pub_wheels_cmd.publish(msg_wheels_cmd)
 
-for i in range(0,80,5):
-    # Put the wheel commands in a message and publish
-    msg_wheels_cmd = WheelsCmdStamped()
-    msg_wheels_cmd.header.stamp = rospy.Time.now()
-    msg_wheels_cmd.vel_right = i/100.0
-    msg_wheels_cmd.vel_left = i/100.0
-    pub_wheels_cmd.publish(msg_wheels_cmd)
-    rospy.sleep(0.4)
+	def StraightCalib(self):
+		rospy.loginfo("Straight calibration starts")
 
-# Put the wheel commands in a message and publish
-msg_wheels_cmd = WheelsCmdStamped()
-msg_wheels_cmd.header.stamp = rospy.Time.now()
-msg_wheels_cmd.vel_right =0.0
-msg_wheels_cmd.vel_left =0.0
-pub_wheels_cmd.publish(msg_wheels_cmd)
-
-rospy.loginfo("Please replace your Duckietown at the beginning of the track; calibration will restart in 10 seconds")
-rospy.sleep(10)
-
-k = 0.2
-alpha = 0.4
-omega = 0.02
-phi =  pi
-for i in range(0,1000,1):
-    # Put the wheel commands in a message and publish
-    msg_wheels_cmd = WheelsCmdStamped()
-    msg_wheels_cmd.header.stamp = rospy.Time.now()
-    msg_wheels_cmd.vel_right = k*(1+alpha*sin(omega*i))
-    msg_wheels_cmd.vel_left = k*(1+alpha*sin(omega*i+phi))
-    pub_wheels_cmd.publish(msg_wheels_cmd)
-
-    rospy.sleep(0.01)
-
-# Put the wheel commands in a message and publish
-msg_wheels_cmd = WheelsCmdStamped()
-msg_wheels_cmd.header.stamp = rospy.Time.now()
-msg_wheels_cmd.vel_right =0.0
-msg_wheels_cmd.vel_left =0.0
-pub_wheels_cmd.publish(msg_wheels_cmd)
+		for i in range(0,self.vFinStraight,self.stepStraight):
+			self.sendCommand(i/100.0, i/100.0)
+			rospy.sleep(self.durationStraight)
+		self.sendCommand(0, 0)
 
 
-rospy.loginfo("Calibration finished")
+	def SinCalib(self):
+		rospy.loginfo("Sin calibration starts") 
+		k = 0.2
+		alpha = 0.4
+		omega = 0.02
+		phi =  pi
+		for i in range(0,1000,1):
+			self.sendCommand(k*(1+alpha*sin(omega*i)),k*(1+alpha*sin(omega*i+phi))) 		
+			rospy.sleep(0.01)
+		
+		self.sendCommand(0,0)
+
+
+if __name__ == '__main__':
+
+	calib=calibration()
+
+	calib.StraightCalib()
+	rospy.loginfo("Replace your Duckietown at the beginning of the track, the calibration will restart in 10 seconds")
+	rospy.sleep(10)
+	calib.SinCalib()
+	rospy.loginfo("Calibration finished")
+
 
