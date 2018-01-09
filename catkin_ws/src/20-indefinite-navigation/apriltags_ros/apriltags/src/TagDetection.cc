583ef2715fd49a8cd557734b521848d324b4f037
BrettRStephens <sbrett@student.ethz.ch>
2018-01-09 18:49:19 +0100

    assigned rvec values

--- catkin_ws/src/20-indefinite-navigation/apriltags_ros/apriltags/src/TagDetection.cc
+++ catkin_ws/src/20-indefinite-navigation/apriltags_ros/apriltags/src/TagDetection.cc
@@ -101,9 +101,13 @@ Eigen::Matrix4d TagDetection::getRelativeTransform(double tag_size, double fx, d
   cv::Matx33d r;
   std::cout<<"break 1"<<std::endl;
   std::cout<<"r = "<<r<<std::endl;
-  rvec.at<double>(0) = -2.820025629275124;
-  rvec.at<double>(1) = 0.05840543250558225;
-  rvec.at<double>(2) = -0.6906929605516307;
+  //rvec.at<double>(0) = -2.820025629275124;
+  //rvec.at<double>(1) = 0.05840543250558225;
+  //rvec.at<double>(2) = -0.6906929605516307;
+  rvec[0] = -2.820025629275124;
+  rvec[1] = 0.05840543250558225;
+  rvec[2] = -0.6906929605516307;
+
   std::cout<<"rvec = "<<rvec<<std::endl;
   cv::Rodrigues(rvec, r);
   std::cout<<"break 2"<<std::endl;
