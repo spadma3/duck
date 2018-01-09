7e52c8d6f2ecb9542aea4b26570a0cfe02387045
NilsFunk <funkn@student.ethz.ch>
2018-01-09 12:13:58 +0100

    april tag launch file without post processing

--- catkin_ws/src/20-indefinite-navigation/apriltags_ros/apriltags_ros/src/apriltag_detector.cpp
+++ catkin_ws/src/20-indefinite-navigation/apriltags_ros/apriltags_ros/src/apriltag_detector.cpp
@@ -52,8 +52,8 @@ namespace apriltags_ros{
   void AprilTagDetector::imageCb(const sensor_msgs::ImageConstPtr& msg,const sensor_msgs::CameraInfoConstPtr& cam_info){
     cv_bridge::CvImagePtr cv_ptr;
     try{
-      //cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-      cv_ptr = cv_bridge::toCvShare(msg, enc::BGR8);
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+      //cv_ptr = cv_bridge::toCvShare(msg, enc::BGR8);
     }
     catch (cv_bridge::Exception& e){
       ROS_ERROR("cv_bridge exception: %s", e.what());
