23acdf461d5feffa6e6f774a00f4b2b2036d6f59
BrettRStephens <sbrett@student.ethz.ch>
2018-01-10 18:27:49 +0100

    some changes to localization

--- catkin_ws/src/30-localization-and-planning/localization/src/localization_node.py
+++ catkin_ws/src/30-localization-and-planning/localization/src/localization_node.py
@@ -47,8 +47,9 @@ class LocalizationNode(object):
         avg = PoseAverage.PoseAverage()
         for tag in msg_tag.detections:
             try:
+                print "test"
                 Tt_w = self.tfbuf.lookup_transform(self.world_frame, "tag_{id}".format(id=tag.id), rospy.Time(), rospy.Duration(1))
-                print Tt_w.transform
+                print (Tt_w.transform)
                 Mtbase_w=self.transform_to_matrix(Tt_w.transform)
                 Mt_tbase = tr.concatenate_matrices(tr.translation_matrix((0,0,0.17)), tr.euler_matrix(0,0,np.pi))
                 Mt_w = tr.concatenate_matrices(Mtbase_w,Mt_tbase)
