3355a1d4d865fee66414ee1b93b2d8af10eb3bff
dario brescianini <bdario@ethz.ch>
2018-01-12 15:00:46 +0100

    bla

--- catkin_ws/src/20-indefinite-navigation/intersection_navigation/src/intersection_navigation.py
+++ catkin_ws/src/20-indefinite-navigation/intersection_navigation/src/intersection_navigation.py
@@ -167,7 +167,7 @@ class IntersectionNavigation(object):
 
                 msg2 = Twist2DStamped()
                 msg2.header.stamp = rospy.Time.now()
-                if 4.0 < (rospy.Time.now() - self.debug_start).to_sec() and (rospy.Time.now() - self.debug_start).to_sec() < 10.0:
+                if 4.0 < (rospy.Time.now() - self.debug_start).to_sec() and (rospy.Time.now() - self.debug_start).to_sec() < 60.0:
                     msg2.v = 0.1
                     msg2.omega = 0.1/0.4
                 else:
