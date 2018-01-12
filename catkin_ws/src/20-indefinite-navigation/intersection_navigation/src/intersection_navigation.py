b47efe3ef3d224e5bcc5a778cd4b4068df83ffda
dario brescianini <bdario@ethz.ch>
2018-01-12 12:45:48 +0100

    localizer outside of node

--- catkin_ws/src/20-indefinite-navigation/intersection_navigation/src/intersection_navigation.py
+++ catkin_ws/src/20-indefinite-navigation/intersection_navigation/src/intersection_navigation.py
@@ -323,7 +323,7 @@ class IntersectionNavigation(object):
 
     def PoseCallback(self, msg):
         pose_meas = np.array([msg.x, msg.y, msg.theta])
-        self.poseEstimator.UpdateWithPoseMeasurement(msg, 1e-5*np.diag([1.0,1.0,1.0]), msg.header.stamp)
+        self.poseEstimator.UpdateWithPoseMeasurement(pose_meas, 1e-5*np.diag([1.0,1.0,1.0]), msg.header.stamp)
 
 
     def CmdCallback(self, msg):
