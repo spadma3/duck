1910a14db8f2f8caeedc04bd8907977893e4320c
NilsFunk <funkn@student.ethz.ch>
2018-01-15 19:43:39 +0100

    add sign to curveture

--- catkin_ws/src/50-misc-additional-functionality/parking/src/path_planning_node.py
+++ catkin_ws/src/50-misc-additional-functionality/parking/src/path_planning_node.py
@@ -142,6 +142,8 @@ class parkingPathPlanner():
             c_ref = 0.0
         else:
             c_ref = 1.0/curvature
+            if sign(pyaw[idx_proj-1]-pyaw[idx_proj]) > 0:
+                c_ref = -c_ref
 
         # differential var_heading
         theta_est = self.yaw_act - self.pyaw[idx_proj]
