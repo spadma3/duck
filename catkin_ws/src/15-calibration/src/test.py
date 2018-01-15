c9eea5a24da8646980aa47ec47641d875cf89b9b
Manuel Dangel <mdangel@student.ethz.ch>
2018-01-15 21:40:57 +0100

    Changed Test (Validation) File

--- catkin_ws/src/15-calibration/src/test.py
+++ catkin_ws/src/15-calibration/src/test.py
@@ -17,7 +17,7 @@ class calibration:
                 self.pub_wheels_cmd = rospy.Publisher(publisher,Twist2DStamped,queue_size=1)
 
 #                self.omega = rospy.get_param("~omega")
-		self.omega=0.0
+		self.omega=2.0*pi / 5.0
 
 	def sendCommand(self, vel_right, vel_left):
 		# Put the wheel commands in a message and publish
@@ -28,15 +28,15 @@ class calibration:
 		self.pub_wheels_cmd.publish(msg_wheels_cmd)
 
         def straight(self):
-                for n in range(0,200):
+                for n in range(0,10):
                         self.sendCommand(0.2, 0)
-                        rospy.sleep(0.01)
+                        rospy.sleep(0.5)
                 self.sendCommand(0, 0)
 
         def turn(self):
-                for i in range(0,1000):
+                for i in range(0,10):
                         self.sendCommand(0.2,self.omega)
-                        rospy.sleep(0.01)
+                        rospy.sleep(0.5)
                 self.sendCommand(0,0)
 
 
@@ -44,7 +44,9 @@ if __name__ == '__main__':
         calib=calibration()
         rospy.loginfo("straight")
         calib.straight()
+        rospy.loginfo("prepare for circle")
         rospy.sleep(5)
+        rospy.loginfo("circle")
         calib.turn()
 
         #os.system("rosnode kill /record")
