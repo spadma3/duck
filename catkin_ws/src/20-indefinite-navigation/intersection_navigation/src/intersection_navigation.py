a054bb3b6accd3b19455dbca1a1e969724b928b3
dario brescianini <bdario@ethz.ch>
2018-01-15 17:57:09 +0100

    fixed tight turns

--- catkin_ws/src/20-indefinite-navigation/intersection_navigation/src/intersection_navigation.py
+++ catkin_ws/src/20-indefinite-navigation/intersection_navigation/src/intersection_navigation.py
@@ -394,9 +394,9 @@ class IntersectionNavigation(object):
             self.pub_intersection_pose_img.publish(msg_out)
 
     def PoseCallback(self, msg):
-        '''pose_meas = np.array([msg.x, msg.y, msg.theta])
+        pose_meas = np.array([msg.x, msg.y, msg.theta])
         self.poseEstimator.UpdateWithPoseMeasurement(pose_meas, 1e-7*np.diag([1.0,1.0,1.0]), msg.header.stamp)
-        pass'''
+        pass
 
 
     def CmdCallback(self, msg):
