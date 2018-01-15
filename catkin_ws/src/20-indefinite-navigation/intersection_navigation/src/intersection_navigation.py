5cffba9a439dcb0a124977acf92de0b513062847
dario brescianini <bdario@ethz.ch>
2018-01-15 17:27:58 +0100

    fixed tight turns

--- catkin_ws/src/20-indefinite-navigation/intersection_navigation/src/intersection_navigation.py
+++ catkin_ws/src/20-indefinite-navigation/intersection_navigation/src/intersection_navigation.py
@@ -210,8 +210,8 @@ class IntersectionNavigation(object):
                         msg_cmds.v = 0.061 + 0.5 * math.fabs(omega) * 0.1
                         self.alpha = self.alpha*msg_cmds.v/0.15
 
-                    msg_cmds.v = msg_cmds.cmds_v * 1.467
-                    msg_cmds.omega =  msg_cmds.omega* 4.24
+                    msg_cmds.v = msg_cmds.v * 1.467
+                    msg_cmds.omega = msg_cmds.omega * 4.24
 
                     self.s = self.s + self.alpha*(rospy.Time.now() - self.debug_time).to_sec()
 
