57947fa41f3311205461e1bc19c498cc8ef2761d
NilsFunk <funkn@student.ethz.ch>
2018-01-15 11:56:04 +0100

    add path plotting

--- catkin_ws/src/50-misc-additional-functionality/parking/src/path_planning_node.py
+++ catkin_ws/src/50-misc-additional-functionality/parking/src/path_planning_node.py
@@ -357,7 +357,7 @@ class parkingPathPlanner():
     def do_plotting(self, start_x, start_y, start_yaw, end_x, end_y, end_yaw, end_number, px, py, objects,
                     obstacles, found_path):
         if close_itself:
-            plt.clf()
+            #plt.clf()
         fig, ax = plt.subplots()
         if found_path:
             plt.plot(px, py, 'g-', lw=3)
@@ -405,10 +405,10 @@ class parkingPathPlanner():
         ax.add_patch(patches.Rectangle((0.0, 0.0), lot_width, lot_height, fc=(0.3, 0.3, 0.3), fill=False))
 
         if close_itself:
-            plt.draw()
-            plt.pause(pause_per_path)
+            #plt.draw()
+            #plt.pause(pause_per_path)
         else:
-            plt.show()
+            #plt.show()
 
         if save_figures:
             dic = {True: 'driveable', False: 'collision'}
