9a432971fcd5950e249a991a7ff3d0e41f7d571d
dario brescianini <bdario@ethz.ch>
2018-01-15 20:13:42 +0100

    bla

--- catkin_ws/src/20-indefinite-navigation/intersection_navigation/src/intersection_navigation.py
+++ catkin_ws/src/20-indefinite-navigation/intersection_navigation/src/intersection_navigation.py
@@ -393,9 +393,17 @@ class IntersectionNavigation(object):
             msg_out.img = msg
             self.pub_intersection_pose_img.publish(msg_out)
 
+            print('----------')
+            print('pose pred')
+            print(msg.header.stamp)
+            print(pose_pred)
+
     def PoseCallback(self, msg):
         pose_meas = np.array([msg.x, msg.y, msg.theta])
         self.poseEstimator.UpdateWithPoseMeasurement(pose_meas, 1e-7*np.diag([1.0,1.0,1.0]), msg.header.stamp)
+        print('pose meas')
+        print(msg.header.stamp)
+        print(pose_meas)
 
 
     def CmdCallback(self, msg):
