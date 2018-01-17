bd5db6bea640378a9854d4e602af11732303c3e0
Theodore Koutros <koutrost@student.ethz.ch>
2018-01-17 11:11:50 +0100

    debugging

--- catkin_ws/src/20-indefinite-navigation/intersection_navigation/scripts/at_intersection_switch_turntype.py
+++ catkin_ws/src/20-indefinite-navigation/intersection_navigation/scripts/at_intersection_switch_turntype.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 import rospy
-from duckietown_msgs.msg import BoolStamped
+from duckietown_msgs.msg import BoolStamped, Int16
 
 if __name__ == '__main__':
     rospy.init_node('dummy', anonymous=True)
     pub = rospy.Publisher('bluck/intersection_navigation_node/switch', BoolStamped, queue_size=1)
-	 pub2 = rospy.Publisher('bluck/intersection_navigation_node/turn_type', Int16, queue_size=1)
+	pub2 = rospy.Publisher('bluck/intersection_navigation_node/turn_type', Int16, queue_size=1)
 
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
