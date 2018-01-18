0f96ce054d99fa34d328af6e5c8e0f42b2f5c83c
BrettRStephens <sbrett@student.ethz.ch>
2018-01-18 15:38:34 +0100

    updates

--- catkin_ws/src/50-misc-additional-functionality/parking/src/path_planning_node.py
+++ catkin_ws/src/50-misc-additional-functionality/parking/src/path_planning_node.py
@@ -132,7 +132,7 @@ class parkingPathPlanner():
             state.v_ref = self.v_ref
             self.sample_state_pub.publish(state)
         end = rospy.get_rostime()
-        rospy.logerr("Pathplanning/Sample Callback [micros]: ", (end.nsecs-begin.nsecs)/1000)
+        rospy.logerr("Pathplanning/Sample Callback [micros]: %s" %((end.nsecs-begin.nsecs)/1000))
 
     def parking_active_callback(self,event):
         #rospy.loginfo("in parking_active_callback")
