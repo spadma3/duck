982b029bfd9236d7726eb7b6b56f8b6dcda231de
dario brescianini <bdario@ethz.ch>
2018-01-19 16:17:06 +0100

    blub

--- catkin_ws/src/10-lane-control/lane_control/scripts/lane_controller_node.py
+++ catkin_ws/src/10-lane-control/lane_control/scripts/lane_controller_node.py
@@ -78,7 +78,7 @@ class lane_controller(object):
         self.v_bar_gain_ref = 0.3*self.velocity_to_m_per_s
         v_bar_fallback = 0.3  # nominal speed, 0.3m/s
         k_theta_fallback = (-20)
-        k_d_fallback = - (k_theta_fallback ** 2) / ( 40.0 * self.v_bar_gain_ref)
+        k_d_fallback = - (k_theta_fallback ** 2) / ( 0.4 * self.v_bar_gain_ref)
         theta_thres_fallback = math.pi / 6
         d_thres_fallback = math.fabs(k_theta_fallback / k_d_fallback) * theta_thres_fallback
         d_offset_fallback = 0.0
