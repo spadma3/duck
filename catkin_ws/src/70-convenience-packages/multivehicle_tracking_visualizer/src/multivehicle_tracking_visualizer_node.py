27d8c22c9a68d3d5ff1c444b1e85746b059a3248
Manfred Diaz <takeitallsource@gmail.com>
2018-01-22 21:49:33 -0500

    [FIXED] Improved visualization for demo.

--- catkin_ws/src/70-convenience-packages/multivehicle_tracking_visualizer/src/multivehicle_tracking_visualizer_node.py
+++ catkin_ws/src/70-convenience-packages/multivehicle_tracking_visualizer/src/multivehicle_tracking_visualizer_node.py
@@ -5,10 +5,11 @@ import threading
 
 import math
 import rospy
-import tf2_py
+import tf
 from multivehicle_tracker.msg import Tracklet, TrackletList
 from std_msgs.msg import ColorRGBA
 from visualization_msgs.msg import Marker, MarkerArray
+from duckietown_msgs.msg import LanePose
 
 
 class MultiVehicleTrackerVisualizerNode:
@@ -21,14 +22,56 @@ class MultiVehicleTrackerVisualizerNode:
         self.rand_gen = uniform
         # Subscribers
         self.sub_detections = rospy.Subscriber("~tracking", TrackletList, self.on_tracklets_received, queue_size=1)
+        self.pose_detection = rospy.Subscriber("~pose", LanePose, self.on_pose, queue_size=1)
         # Publishers
         self.pub_tracklet = rospy.Publisher("~tracking_visualizations", MarkerArray, queue_size=1)
 
-        self.pose_timer = rospy.Timer(rospy.Duration.from_sec(1), self.on_pose)
+    def create_self_marker(self, lane_pose_msg):
+        marker = self.create_colored_marker_msg((1.0, 1.0, 1.0), None, identifier=429496729)
+        yaw_quat = tf.transformations.quaternion_about_axis(-lane_pose_msg.phi, [0, 0, 1])
+        marker.pose.orientation.x = yaw_quat[0]
+        marker.pose.orientation.y = yaw_quat[1]
+        marker.pose.orientation.z = yaw_quat[2]
+        marker.pose.orientation.w = yaw_quat[3]
 
-    def on_pose(self, event):
+        marker.pose.position.x = 0.0
+        marker.pose.position.y = -lane_pose_msg.d
+        marker.pose.position.z = 0.0
+
+        return marker
+
+    def create_name_marker(self):
+        marker = Marker()
+
+        marker.header.frame_id = self.veh_name
+        marker.id = 10000000
+        marker.action = Marker.ADD
+        marker.type = Marker.TEXT_VIEW_FACING
+        marker.text = self.veh_name
+        marker.color = ColorRGBA(r=0, g=0.7, b=0.7, a=0.8)
+        marker.pose.orientation.x = 0.0
+        marker.pose.orientation.y = 0.0
+        marker.pose.orientation.z = 0.0
+        marker.pose.orientation.w = 1.0
+        marker.pose.position.x = 0.0
+        marker.pose.position.y = 0.0
+        marker.pose.position.z = 0.2
+        marker.scale.x = 0.3
+        marker.scale.y = 0.3
+        marker.scale.z = 0.1
+        # marker.lifetime = rospy.Duration.from_sec(0.2)
+
+        return marker
+
+
+    def on_pose(self, lane_pose_msg):
         reference_msg = MarkerArray()
-        reference_msg.markers.append(self.create_colored_marker_msg((1.0, 1.0, 1.0), None, identifier=429496729))
+        self_marker = self.create_self_marker(lane_pose_msg)
+        text_marker = self.create_name_marker()
+
+        reference_msg.markers.append(self_marker)
+        reference_msg.markers.append(text_marker)
+
         self.pub_tracklet.publish(reference_msg)
 
     def create_colored_marker_msg(self, color, tracklet, identifier=None):
@@ -38,7 +81,7 @@ class MultiVehicleTrackerVisualizerNode:
         marker.header.frame_id = self.veh_name
         if identifier is None:  # drawing a particle
             marker.id = randint(4098, 1000000)
-            marker.lifetime = rospy.Duration.from_sec(5.0)
+            marker.lifetime = rospy.Duration.from_sec(1.0)
             marker.type = Marker.SPHERE
             marker.scale.x = math.sqrt(tracklet.sigma_x)
             marker.scale.y = math.sqrt(tracklet.sigma_y)
@@ -50,6 +93,7 @@ class MultiVehicleTrackerVisualizerNode:
         else:
             marker.id = identifier
             marker.type = Marker.MESH_RESOURCE
+            marker.lifetime = rospy.Duration.from_sec(0.5)
             marker.mesh_resource = "package://multivehicle_tracking_visualizer/meshes/duckiebot.dae"
             marker.scale.x = 1.0
             marker.scale.y = 1.0
