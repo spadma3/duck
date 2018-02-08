71b11a39be5c8429e150bd379996b537d0df35a7
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2018-02-08 17:21:17 -0500

    Changed image size to 128x128

--- catkin_ws/src/80-deep-learning/transfer_learning/src/resize_node.py
+++ catkin_ws/src/80-deep-learning/transfer_learning/src/resize_node.py
@@ -15,22 +15,21 @@ class ResizeNode(object):
         self.sub = rospy.Subscriber("~image",Image,self.cbImg, queue_size=1)
         self.pub2 = rospy.Publisher("~image_cropped",Image, queue_size=1)
         self.pub = rospy.Publisher("~image_resize",Image, queue_size=1)
-        
-        
+
+
     def cbImg(self,msg):
 
         img=bgr_from_imgmsg(msg)
         borders = [25, 25, 25, 25]
         img_cropped = crop_image(img, borders)
-        img_resized = d8_image_resize_no_interpolation(img_cropped,[64,64])
+        img_resized = d8_image_resize_no_interpolation(img_cropped,[128,128])
         img_msg = d8n_image_msg_from_cv_image(img_resized,'bgr8')
         img_msg2 = d8n_image_msg_from_cv_image(img_cropped,'bgr8')
         self.pub.publish(img_msg)
         self.pub2.publish(img_msg2)
 
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     rospy.init_node('resize_node',anonymous=False)
     node = ResizeNode()
     rospy.spin()
-
