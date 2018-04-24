0ef6eb80d5538d28e090198c3e3fde9708c72585
Simon Bing/Megacity Team <bings@student.ethz.ch>
2018-04-24 09:06:44 +0200

    Added custom megacity launch file (Julien)

--- Makefiles/Makefile.megacity.mk
+++ Makefiles/Makefile.megacity.mk
@@ -1,5 +1,5 @@
 megacity: check-environment
-	bash -c "source environment.sh; source set_ros_master.sh; source set_vehicle_name.sh; roslaunch duckietown_demos indefinite_navigation.launch"
+	bash -c "source environment.sh; source set_ros_master.sh; source set_vehicle_name.sh; roslaunch duckietown_demos megacity.launch"
 
 tcp-server: check-environment
 	bash -c "source environment.sh; source set_ros_master.sh; source set_vehicle_name.sh; roslaunch duckietown_demos tcp_server.launch"
