47f94db3878d64b5d8f951f71278144ef8518c5e
Eric Lu <eric565648@gmail.com>
2018-04-27 16:13:28 +0200

    remove thread in bot_detector_node, solve some software proplem. Not done yet

--- catkin_ws/src/30-localization-and-planning/auto_localization/src/bot_detector_node.py
+++ catkin_ws/src/30-localization-and-planning/auto_localization/src/bot_detector_node.py
@@ -1,10 +1,10 @@
+#!/usr/bin/env python
 from cv_bridge import CvBridge, CvBridgeError
 from duckietown_msgs.msg import (AntiInstagramTransform, BoolStamped, Segment,
     SegmentList, Vector2D)
 from duckietown_utils.instantiate_utils import instantiate
 from duckietown_utils.jpg import image_cv_from_jpg
 from sensor_msgs.msg import CompressedImage, Image
-from std_msgs import Bool
 
 import cv2
 import rospy
@@ -20,39 +20,36 @@ class BotDetectorNode(object):
 	def __init__(self):
 		self.node_name = "BotDetectorNode"
 
-		#Thread Lock
-		self.thread_lock = threading.lock()
-
 		#Constructor of bot detector
 		self.bridge = CvBridge()
 
 		self.active  = True
 
-		self.stats = Stats()
-
 		#Only be verbose every 10 cycles
 		self.intermittent_intervel = 100
 		self.intermittent_counter = 0
-
-		# Color Correction
 
 		# these will be added if it becomes verbose
 
 		self.detector = None
 		self.verbose = None
-		self.updateParams(None)
+		#self.updateParams(None)
 
 		#Publisher
-		self.pub_result = rospy.Publisher("~bot_existence", Bool, queue_size=1)
+		self.pub_result = rospy.Publisher("~bot_existence", BoolStamped, queue_size=1)
 
 		#Subscriber
-		self.sub_image = rospy.Subscriber("~image", CompressedImage, self.cbImage, queue_size=1)
+		self.sub_image = rospy.Subscriber("image", Image, self.cbImage, queue_size=1)
 		self.subswitch = rospy.Subscriber("~switch", BoolStamped, self.cbSwitch, queue_size=1)
+
+		#Record the first number of sequence
+		self.first_seq = -1
 
 		rospy.loginfo("[%s] Initialized (verbose = %s)" %(self.node_name, self.verbose))
 
-		rospy.Timer(rospy.Duration.from_sec(2.0), self.updateParams)
+		#rospy.Timer(rospy.Duration.from_sec(2.0), self.updateParams)
 
+	'''
 	def updateParams(self):
 		old_verbose = self.verbose
 		self.verbose = rospy.get_param('~verbose', True)
@@ -62,72 +59,46 @@ class BotDetectorNode(object):
 
 		self.image_size = rospy.get_param('~img_size')
 		self.top_cutoff = rospy.get_param('~top_cutoff')
+	'''
 
 	def cbSwitch(self, switch_msg):
 		self.active = switch_msg.data
 
 	def cbImage(self, image_msg):
-		self.stats.received()
 
 		if not self.active:
 			return
-		# Start Daemon thread to process the image
-		thread = threading.Thread(target=self.processImage, args=(image_msg,))
-		thread.setDaemon(True)
-		thread.start()
-		# Returns rightaway
+		self.processImage(image_msg)
 
 	def loginfo(self, s):
 		rospy.loginfo('[%s] %s' % (self.node_name, s))
 
-	def intermittent_log_now(self):
-		return self.intermittent_counter % self.intermittent_intervel == 1
-
-	def intermittent_log(self):
-		if not intermittent_log_now:
-			return
-		self.loginfo('%3d:%s' % (self.intermittent_counter, s))
-
 	def processImage(self, image_msg):
-		if not self.thread_lock.acquire(False):
-			self.stats.skipped()
-			#Return if the thread is locked
-			return
 
-		try:
-			self.processImage_(image_msg)
-		finally:
-			self.thread_lock.release()
-
-	def processImage_(self, image_msg):
-		
-		self.stats.processed()
-
-		if self.intermittent_log_now():
-			self.intermittent_log(self.stats.info())
-			self.stats.reset()
-
-		self.intermittent_counter += 1
-
+		'''
 		# Decode from compressed image with OpenCV
 		try:
 			image_cv = image_cv_from_jpg(image_msg.data)
 		except ValueError as e:
 			self.loginfo('Could not load image: %s' % e)
 			return
+		'''
+		# Record the first number of seq
+		if self.first_seq == -1:
+			self.first_seq = image_msg.header.seq
 
-		frame_id = image_msg.header.frame_id
-		if frame_id <= BG_SAMPLE:
+		n = image_msg.header.seq - self.first_seq
+		if n <= BG_SAMPLE:
 			#Compute background with the first 1000 images
-			if frame_id == 1:
-				self.background = image_cv
+			if n == 0:
+				self.background = image_cv.data
 				self.bg_b, self.bg_g, self.bg_r = cv2.split(self.background)
 			else:
 				self.bg_b, self.bg_g, self.bg_r = self.getMidBackground(self.bg_b, self.bg_g, self.bg_r, image_cv)
 
 			return
 
-		elif frame_id == (BG_SAMPLE+1)
+		elif n == (BG_SAMPLE+1):
 			self.background = cv2.merge(self.bg_b[:, :, BG_SAMPLE/2], self.bg_g[:, :, BG_SAMPLE/2], self.bg_r[:, :, BG_SAMPLE/2])
 
 		self.bg_sum = sum(self.background)
@@ -136,10 +107,14 @@ class BotDetectorNode(object):
 		self.loginfo('Background sum: %d' % (self.bg_sum))
 		self.loginfo('Image sum: %d' % (self.img_sum))
 
+		result = BoolStamped()
+		result.header = image_msg.header
 		if (self.bg_sum - BG_WIDTH/2) <= self.img_sum <= (self.bg_sum + BG_WIDTH/2):
-			self.pub_result.Publish(True)
+			result.data = True
 		else:
-			self.pub_result.Publish(False)
+			result.data = False
+			
+		self.pub_result.Publish(result)
 
 	def getMidBackground(self, bg_b, bg_g, bg_r, img):
 		
@@ -166,6 +141,7 @@ class BotDetectorNode(object):
 	def on_Shutdown(self):
 		self.loginfo("Shutdown.")
 
+'''
 class Stats(object):
 	"""docstring for Stats"""
 	def __init__(self):
@@ -205,6 +181,7 @@ class Stats(object):
 
 		m = 'In the last %.1f s: received %d (%s) processed %d (%s) skipped %d (%s) (%1.f%%)' % (delta, self.nreceived, fps(self.nreceived), self.nprocessed, fps(self.processed), self.nskipped, fps(self.nskipped), skipped_perc)
 		return m 
+'''
 		
 
 
