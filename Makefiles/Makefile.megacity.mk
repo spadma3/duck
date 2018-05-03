223ad225a7551ced2c33e44eca1f2b780ef1e2a5
Simon Bing/Megacity Team <bings@student.ethz.ch>
2018-05-03 16:55:17 +0200

    Making parallel autonomy work

--- Makefiles/Makefile.megacity.mk
+++ Makefiles/Makefile.megacity.mk
@@ -6,3 +6,6 @@ tcp-server: check-environment
 
 virjoy-%: check-environment
 	bash -c "source environment.sh; source set_ros_master.sh $*; source set_vehicle_name.sh $*; python misc/virtualJoy/virtualJoy.py"
+
+parallel-autonomy: check-environment
+	bash -c "source environment.sh; source set_ros_master.sh; source set_vehicle_name.sh; roslaunch duckietown_demos parallel_autonomy.launch"
