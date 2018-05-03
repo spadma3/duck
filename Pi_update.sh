3877a79d5e1fad18f00a20311b998f5e4bf1d9f2
Josefine Quack <Josefine Quack>
2018-05-03 13:17:34 +0200

    Include Script to Update from B to B+

---
+++ Pi_update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt update
+sudo rm /var/lib/apt/lists/lock
+sudo rm /var/cache/apt/archives/lock
+sudo rm /var/lib/dpkg/lock
+sudo apt -y dist-upgrade
+sudo BRANCH=stable rpi-update
+sudo apt update
+sudo apt dist-upgrade
+
+cd /lib
+sudo wget "code.julien.li/firmware.tar.gz"
+sudo tar -zxvf firmware.tar.gz
+sudo rm firmware.tar.gz
+sudo reboot
