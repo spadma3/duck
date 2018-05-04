ba3fe174c539b7a254fc9462aaef3209a9f307bf
Jose <q1337@hackermail.com>
2018-05-04 10:41:25 +0200

    Remove any lock at the start

--- Pi_update.sh
+++ Pi_update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+sudo rm /var/lib/apt/lists/lock
+sudo rm /var/cache/apt/archives/lock
+sudo rm /var/lib/dpkg/lock
 sudo apt update
 sudo rm /var/lib/apt/lists/lock
 sudo rm /var/cache/apt/archives/lock
