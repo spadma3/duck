5cea850e4e4c066cf2fd364c492c26268a4d802f
Eric Lu <eric565648@gmail.com>
2018-05-25 15:20:23 +0200

    improve full ratio rectification to 13Hz, apriltag detection detect full ratio rectified image

--- catkin_ws/src/05-teleop/pi_camera/src/image_rect_full_ratio.py
+++ catkin_ws/src/05-teleop/pi_camera/src/image_rect_full_ratio.py
@@ -5,7 +5,7 @@ import rospy
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 import numpy as np
-from sensor_msgs.msg import CompressedImage,Image
+from sensor_msgs.msg import CompressedImage,Image,CameraInfo
 from duckietown_msgs.msg import BoolStamped
 
 import duckietown_utils as dtu
@@ -24,29 +24,42 @@ class ImgRectFullRatio(object):
         self.active = True
         self.bridge = CvBridge()
 
-        self.pub_rect = rospy.Publisher("image_rect",Image,queue_size=1)
-        self.sub_compressed_img = rospy.Subscriber("image_raw",Image,self.cbImg,queue_size=1)
-        self.sub_switch = rospy.Subscriber("~switch",BoolStamped, self.cbSwitch, queue_size=1)
+        self.cam_info = None
+        self.gpg = None
 
+        self.pub_rect = rospy.Publisher("image_rect",Image,queue_size=1)
+        self.sub_img = rospy.Subscriber("image_raw",Image,self.cbImg,queue_size=1)
+        self.sub_cam_info = rospy.Subscriber("camera_info",CameraInfo,self.cbCamInfo,queue_size=1)
+        self.sub_switch = rospy.Subscriber("~switch",BoolStamped, self.cbSwitch, queue_size=1)
 
     def cbSwitch(self,switch_msg):
         self.active = switch_msg.data
 
+    def cbCamInfo(self,caminfo_msg):
+        if not self.active:
+            return
+
+        self.cam_info = caminfo_msg
+
+        if self.gpg is None:
+            print "Run initialize gpg"
+            robot_name = rospy.get_namespace()
+            robot_name = robot_name[1:-1]
+            disable_old_homography(robot_name)
+            homography_dummy = get_homography_default()
+            self.gpg = GroundProjectionGeometry(self.cam_info, homography_dummy)
+
     def cbImg(self,msg):
         if not self.active:
+            return
+        if self.cam_info is None:
+            return
+        if self.gpg is None:
             return
 
         cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
 
-        robot_name = rospy.get_namespace()
-        robot_name = robot_name[1:-1]
-        disable_old_homography(robot_name)
-
-        camera_info = get_camera_info_for_robot(robot_name)
-        homography_dummy = get_homography_default()
-        gpg = GroundProjectionGeometry(camera_info, homography_dummy)
-
-        new_matrix, result_img = gpg.rectify_full(cv_image, ratio=1.65)
+        new_matrix, result_img = self.gpg.rectify_full(cv_image, ratio=1.65)
 
         img_msg = self.bridge.cv2_to_imgmsg(result_img, "bgr8")
 
