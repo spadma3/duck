f92aba3018118b0be3d1bdfac70aa8d624e184c5
Simon Bing/Megacity Team <bings@student.ethz.ch>
2018-06-19 16:14:35 +0200

    Intermediate push

--- catkin_ws/src/20-indefinite-navigation/auto_calibration/src/auto_calibration_calculation_node.py
+++ catkin_ws/src/20-indefinite-navigation/auto_calibration/src/auto_calibration_calculation_node.py
@@ -47,17 +47,17 @@ class AutoCalibrationCalculationNode(object):
     def cbFSMState(self,msg):
         if (not self.mode == "CALIBRATING_CALC") and msg.state == "CALIBRATING_CALC":
             # Switch into CALIBRATING_CALC mode
+            self.data_gathering = False
             self.mode = msg.state
             self.triggered = True
             rospy.loginfo("[%s] %s triggered." %(self.node_name,self.mode))
             self.publishControl()
-            self.data_gathering = False
             self.resample()
             self.calibration()
         self.mode = msg.state
 
     #Exit function for calibration calculation
-    def finishCalc(self, event):
+    def finishCalc(self):
         rospy.loginfo("[%s] Calculation finished." %(self.node_name))
         done = BoolStamped()
         done.data = True
@@ -107,13 +107,13 @@ class AutoCalibrationCalculationNode(object):
     #Flag for data gathering
     def cbRecord(self, msg):
         if msg.data:
-            self.data_gathering=msg.data
             self.wheel_motion = np.zeros((1,3))
             self.camera_motion = np.zeros((1,7))
+            self.data_gathering=msg.data
 
-#########################################################################################################
+#########################################################################################################################################################################################
 #Functions used in the calibration of the duckiebot
-#########################################################################################################
+#########################################################################################################################################################################################
 
     #Determines the travel of the Duckiebot using the inputs sent to the motors
     def wheels(self,x,args): #maybe done? --> Need data to check if right
@@ -140,60 +140,37 @@ class AutoCalibrationCalculationNode(object):
 
     #Determines the travel of the Duckiebot camera from Apriltag detections
     def visual_odometry(self,cam_loc,id):
-        #TODO adapt code to have the right output
-
         #Read tag location from yaml file
         tag_loc=self.tags_locations['tag'+str(id)]
 
         #This part can be done outside of the optimization algorithm --> save computation time
         #If more than 1 tag detected, take the average location of the N detections
         #Homogenuous transform World to Tag
-        #The tags are assumed to have 0 rotation in x and y
-        tag_R_world = np.array([[np.cos(tag_loc[5]),-np.sin(tag_loc[5]),0],
-                                [np.sin(tag_loc[5]), np.cos(tag_loc[5]),0],
-                                [0,0,1]])
-        tag_t_world = np.array([[tag_loc[0]],[tag_loc[1]],[tag_loc[2]]])
-        tag_T_world = np.concatenate((np.concatenate((tag_R_world,tag_t_world), axis=1),np.array([[0,0,0,1]])), axis=0)
+        tag_T_world = self.etH(tag_loc)
 
-        cam_loc=np.array([0.2,0.1,0,0,20.0/180.0*3.1415926,3.1415926])
         #Homogenuous transform Tag to Cam
-        tag_Rx_cam = np.array([[1,0,0],
-                               [0,np.cos(cam_loc[3]),-np.sin(cam_loc[3])],
-                               [0,np.sin(cam_loc[3]), np.cos(cam_loc[3])]])
-        tag_Ry_cam = np.array([[ np.cos(cam_loc[4]),0,np.sin(cam_loc[4])],
-                               [0,1,0],
-                               [-np.sin(cam_loc[4]),0,np.cos(cam_loc[4])]])
-        tag_Rz_cam = np.array([[np.cos(cam_loc[5]),-np.sin(cam_loc[5]),0],
-                               [np.sin(cam_loc[5]), np.cos(cam_loc[5]),0],
-                               [0,0,1]])
-        tag_R_cam = np.dot(np.dot(tag_Rz_cam,tag_Ry_cam),tag_Rx_cam)
-        tag_t_cam = np.array([[cam_loc[0]],[cam_loc[1]],[cam_loc[2]]])
-        tag_T_cam = np.concatenate((np.concatenate((tag_R_cam,tag_t_cam), axis=1),np.array([[0,0,0,1]])), axis=0)
+        tag_T_cam = self.etH(cam_loc)
         cam_T_tag = np.linalg.inv(tag_T_cam)
 
         cam_T_world = np.dot(tag_T_world,cam_T_tag)
-        return np.array([1,2,3,4,5,6])
+
+        R = cam_T_world[:3,:3]
+        [roll,pitch,yaw] = self.Rte(R)
+        return np.array([cam_T_world[0][3],cam_T_world[1][3],cam_T_world[2][3],roll,pitch,yaw])
 
     #Determines the travel of the Duckiebot from the movement of its camera
-    def camera(self,x,args):
-        #TODO adapt code to have the right output
+    def camera(self,x,cam_loc):
         #Homogenuous transform from camera to Duckiebot 'center'
-        cam_Rx_bot = np.array([[1,0,0],
-                               [0,np.cos(x[7]),-np.sin(x[7])],
-                               [0,np.sin(x[7]), np.cos(x[7])]])
-        cam_Ry_bot = np.array([[ np.cos(x[8]),0,np.sin(x[8])],
-                               [0,1,0],
-                               [-np.sin(x[8]),0,np.cos(x[8])]])
-        cam_Rz_bot = np.array([[np.cos(x[9]),-np.sin(x[9]),0],
-                               [np.sin(x[9]), np.cos(x[9]),0],
-                               [0,0,1]])
-        cam_R_bot = np.dot(np.dot(cam_Rz_bot,cam_Ry_bot),cam_Rx_bot)
-        cam_t_bot = np.array([[x[4]],[x[5]],[x[6]]])
-        cam_T_bot = np.concatenate((np.concatenate((cam_R_bot,cam_t_bot), axis=1),np.array([[0,0,0,1]])), axis=0)
+        cam_T_bot = self.etH(x[4:])
         bot_T_cam = np.linalg.inv(cam_T_bot)
 
+        #Homogenuous transform World to camera
+        cam_T_world = self.etH(cam_loc)
+
         bot_T_world = np.dot(cam_T_world,bot_T_cam)
-        return np.zeros(6)
+        R = bot_T_world[:3,:3]
+        [roll,pitch,yaw] = self.Rte(R)
+        return np.array([bot_T_world[0][3],bot_T_world[1][3],bot_T_world[2][3],roll,pitch,yaw])
 
     #quaternion to euler, taken from https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles on 05.06.18
     def qte(self, w, x, y, z):
@@ -289,7 +266,6 @@ class AutoCalibrationCalculationNode(object):
             bnds = (b0, b1, b2, b3, b4, b5, b6, b7, b8, b9)
 
             #Array of camera positions and wheel velocities with corresponding timestamp
-            #para=np.ones((9,100))
 
             # optimization
             solution = minimize(self.objective,x0,args=self.para, method='SLSQP',bounds=bnds)
@@ -300,32 +276,40 @@ class AutoCalibrationCalculationNode(object):
     #Objective function of the optimization algorithm
     def objective(self,x,args):
         #Weighting matrix
-        Q=np.identity(6)
+        #Q=np.identity(6)
 
         #estimation by wheels
         est1=self.wheels(x,args)
 
-        #estimation by camera
-        est2=self.camera(x,args)
+        camera_frames = np.size(self.camera_motion,0)
+        camera_pos = np.zeros(camera_frames,6)
+        camera_mov = np.zeros(camera_frames-1,6)
+        for i in range(0,camera_frames):
+            #estimation by camera
+            camera_pos[i]=self.camera(x,self.camera_motion[i])
+            if i>0:
+                camera_mov[i-1]=camera_pos[i]-camera_pos[i-1]
 
-        return np.linalg.norm(np.dot(Q,est1-est2))
+        camera_mov=np.reshape(camera_mov,np.size(camera_mov))
+        #return np.linalg.norm(np.dot(Q,est1-est2))
+        return np.linalg.norm(est1-camera_mov)
 
     #secs and nsecs to secs double
     def toSeconds(self,secs,nsecs):
         return secs + nsecs/1000000000.0
 
-    def resample():
+    def resample(self):
         #delete first row of both arrays, as they are filled with zeros
         self.wheel_motion = np.delete(self.wheel_motion,0,0)
         self.camera_motion = np.delete(self.camera_motion,0,0)
         wheel_entries = np.size(self.wheel_entries,0)
         camera_entries = np.size(self.camera_motion,0)
         #delete last camera entry, to be sure that there are wheel entries coming after it
-        self.camera_motion = np.delete(self.camera_motion,camera_entries-1,0)
+        self.camera_motion = np.delete(self.cametHera_motion,camera_entries-1,0)
         camera_entries=camera_entries-1
         for i in range(0,camera_entries):
             for j in range(0,wheel_entries):
-                if (self.wheel_motion[j][0]>self.camera_motion[i][0]):
+                if (self.wheel_motion[j][0]>=self.camera_motion[i][0]):
                     self.wheel_motion = np.insert(self.wheel_motion,j,[self.camera_motion[i][0],self.wheel_motion[j-1][1],self.wheel_motion[j-1][2]],axis=0)
                     wheel_entries=wheel_entries+1
                     #delete all wheel entries before the first camera entry
@@ -336,6 +320,8 @@ class AutoCalibrationCalculationNode(object):
                     if (i==camera_entries-1):
                         self.wheel_motion = np.delete(self.wheel_motion,slice(j+1,wheel_entries),0)
                     break
+
+        #parameter vector to store all the robots motion (resampeld)
         self.para = self.wheel_motion
 
         self.para = np.append(self.para, np.zeros((np.size(self.para,0),6)),axis=1)
@@ -351,8 +337,50 @@ class AutoCalibrationCalculationNode(object):
                 self.para[i][8] = self.camera_motion[j][6]
                 j=j+1
 
+    #Taken from https://www.learnopencv.com/rotation-matrix-to-euler-angles/ on 19.6.18
+    # Checks if a matrix is a valid rotation matrix.
+    def isRotationMatrix(self,R) :
+        Rt = np.transpose(R)
+        shouldBeIdentity = np.dot(Rt, R)
+        I = np.identity(3, dtype = R.dtype)
+        n = np.linalg.norm(I - shouldBeIdentity)
+        return n < 1e-6
 
+    #Taken from https://www.learnopencv.com/rotation-matrix-to-euler-angles/ on 19.6.18
+    # Calculates rotation matrix to euler angles
+    # The result is the same as MATLAB except the order
+    # of the euler angles ( x and z are swapped ).
+    def Rte(self,R) :
 
+        assert(isRotationMatrix(R))
+        sy = math.sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])
+        singular = sy < 1e-6
+        if not singular :
+            x = math.atan2(R[2,1] , R[2,2])
+            y = math.atan2(-R[2,0], sy)
+            z = math.atan2(R[1,0], R[0,0])
+        else :
+            x = math.atan2(-R[1,2], R[1,1])
+            y = math.atan2(-R[2,0], sy)
+            z = 0
+
+        return x, y, z
+
+    #euler and translation to Homography
+    #para is x,y,z,roll,pitch,yaw
+    def etH(self,para):
+        Rx = np.array([[1,0,0],
+                       [0,np.cos(para[3]),-np.sin(para[3])],
+                       [0,np.sin(para[3]), np.cos(para[3])]])
+        Ry = np.array([[ np.cos(para[4]),0,np.sin(para[4])],
+                       [0,1,0],
+                       [-np.sin(para[4]),0,np.cos(para[4])]])
+        Rz = np.array([[np.cos(para[5]),-np.sin(para[5]),0],
+                       [np.sin(para[5]), np.cos(para[5]),0],
+                       [0,0,1]])
+        R = np.dot(np.dot(Rz,Ry),Rx)
+        t = np.array([[para[0]],[para[1]],[para[2]]])
+        return np.concatenate((np.concatenate((R,t), axis=1),np.array([[0,0,0,1]])), axis=0)
 
     def setupParams(self):
         self.tags_locations = self.setupParam("~tags",0)
