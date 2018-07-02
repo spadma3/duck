cb5bbf6cd0f744cb7f63df58e8573e4cea19600a
Eric Lu <eric565648@gmail.com>
2018-07-02 16:57:33 +0200

    start writing graphs

--- catkin_ws/src/30-localization-and-planning/auto_localization/src/system_calibration.py
+++ catkin_ws/src/30-localization-and-planning/auto_localization/src/system_calibration.py
@@ -70,10 +70,26 @@ class system_calibration(object):
         # https://www.python.org/doc/essays/graphs/
         tag_graph = {}
         for tf in tfs:
-            if not graph.has_key(tf.frame_id):
-                graph{'tf.frame_id'} = []
+            if not tag_graph.has_key(tf.frame_id):
+                tag_graph{'tf.frame_id'} = []
             else:
-                graph{'tf.frame_id'}.append(tf.bot_id)
+                tag_graph{'tf.frame_id'}.append(tf.bot_id)
+
+
+    def find_shortest_path(graph, start, end, path=[]):
+        path = path + [start]
+        if start == end:
+            return path
+        if not graph.has_key(start):
+            return None
+        shortest = None
+        for node in graph[start]:
+            if node not in path:
+                newpath = find_shortest_path(graph, node, end, path)
+                if newpath:
+                    if not shortest or len(newpath) < len(shortest):
+                        shortest = newpath
+        return shortest
 
 
     ## Load Map Data
