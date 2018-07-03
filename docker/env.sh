dbe392242f2c8c5522e4129cac28380d7141fcf3
Ruslan Hristov <rusi@nutonomy.com>
2018-07-03 15:52:13 -0400

    updated env variables

--- docker/env.sh
+++ docker/env.sh
@@ -2,8 +2,8 @@
 
 IP=$(ifconfig wlan0 | grep "inet " | awk -F'[: ]+' '{ print $4 }')
 source /home/software/environment.sh
-# export DUCKIEFLEET_ROOT=/home/duckiefleet
+export DUCKIEFLEET_ROOT=/data/config
 export ROS_MASTER_URI=http://${IP}:11311/
 export ROS_IP=${IP}
-export VEHICLE_NAME=docker
+export VEHICLE_NAME=$HOSTNAME
 cat misc/duckie.art
