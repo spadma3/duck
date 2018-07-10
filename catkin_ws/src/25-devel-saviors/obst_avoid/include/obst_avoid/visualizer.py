79bd1120f6b332202558ce9bd676986a27305591
Dzenan Lapandic <ldzenan@student.ethz.ch>
2018-07-10 12:08:12 +0200

    Added visualization of bird perspective image

--- catkin_ws/src/25-devel-saviors/obst_avoid/include/obst_avoid/visualizer.py
+++ catkin_ws/src/25-devel-saviors/obst_avoid/include/obst_avoid/visualizer.py
@@ -24,11 +24,11 @@ class Visualizer():
         self.detector = Detector(robot_name=self.robot_name)
         #create detector object to get access to all the trafos
 
-			
+
     def visualize_marker(self, obst_list):
     	marker_list=MarkerArray()
-	
-	
+
+
 
    	size = obst_list.poses.__len__()
    	for i in range(0,size):
@@ -57,7 +57,7 @@ class Visualizer():
                 #marker.scale.y = 0.2
 	   	marker.pose.position.x = obst_list.poses[i].position.x
 	   	marker.pose.position.y = obst_list.poses[i].position.y
-	   	marker.pose.position.z = 0 
+	   	marker.pose.position.z = 0
 	   	marker_list.markers.append(marker)
 
     	#print marker_list.markers.__len__()
@@ -66,16 +66,34 @@ class Visualizer():
     def visualize_image(self, image,obst_list):
 
     	size = obst_list.poses.__len__()
-   	for i in range(0,size):
-                top = obst_list.poses[i].orientation.x
-                bottom = obst_list.poses[i].orientation.y
-                left = obst_list.poses[i].orientation.z
-                right = obst_list.poses[i].orientation.w
-                points = np.float32([[left,left,right,right],[top,bottom,bottom,top]])
-                pts = self.detector.bird_view_pixel2real_pic_pixel(points)
-                if (obst_list.poses[i].position.z<0):
-                        cv2.polylines(image,np.int32([np.transpose(pts)]),True,(0,255,0),3)
-                else:
-                        cv2.polylines(image,np.int32([np.transpose(pts)]),True,(255,0,0),3)
-	    
-	return d8_compressed_image_from_cv_image(image[:,:,::-1])
+       	for i in range(0,size):
+                    top = obst_list.poses[i].orientation.x
+                    bottom = obst_list.poses[i].orientation.y
+                    left = obst_list.poses[i].orientation.z
+                    right = obst_list.poses[i].orientation.w
+                    points = np.float32([[left,left,right,right],[top,bottom,bottom,top]])
+                    pts = self.detector.bird_view_pixel2real_pic_pixel(points)
+                    if (obst_list.poses[i].position.z<0):
+                            cv2.polylines(image,np.int32([np.transpose(pts)]),True,(0,255,0),3)
+                    else:
+                            cv2.polylines(image,np.int32([np.transpose(pts)]),True,(255,0,0),3)
+
+    	return d8_compressed_image_from_cv_image(image[:,:,::-1])
+
+    def visualize_bird_perspective(self, image,obst_list):
+        #hsv = cv2.cvtColor(image[self.crop:, :, :], cv2.COLOR_RGB2HSV)
+    
+        bird_img = cv2.warpPerspective(image, self.detector.M, (self.detector.img_width, self.detector.img_height))
+    	size = obst_list.poses.__len__()
+       	for i in range(0,size):
+                    top = obst_list.poses[i].orientation.x
+                    bottom = obst_list.poses[i].orientation.y
+                    left = obst_list.poses[i].orientation.z
+                    right = obst_list.poses[i].orientation.w
+                    points = np.float32([[left,left,right,right],[top,bottom,bottom,top]])
+                    if (obst_list.poses[i].position.z<0):
+                            cv2.polylines(bird_img,np.int32([np.transpose(points)]),True,(0,255,0),3)
+                    else:
+                            cv2.polylines(bird_img,np.int32([np.transpose(points)]),True,(255,0,0),3)
+
+    	return d8_compressed_image_from_cv_image(bird_img[:,:,::-1])
