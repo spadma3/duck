ee02ce38ce109551ee2ec9147fb2905ca7baa9d6
Simon Bing/Megacity Team <bings@student.ethz.ch>
2018-07-12 13:12:40 +0200

    Added city-modularity

--- Makefiles/Makefile.megacity.mk
+++ Makefiles/Makefile.megacity.mk
@@ -1,6 +1,9 @@
 megacity: check-environment
 	bash -c "source environment.sh; source set_ros_master.sh; source set_vehicle_name.sh; roslaunch duckietown_demos megacity.launch"
 
+megacity-%: check-environment
+	bash -c "source environment.sh; source set_ros_master.sh; source set_vehicle_name.sh; roslaunch duckietown_demos megacity.launch city:=$*"
+
 megacity-no-maintenance: check-environment
 	bash -c "source environment.sh; source set_ros_master.sh; source set_vehicle_name.sh; roslaunch duckietown_demos megacity-no-maintenance.launch"
 
