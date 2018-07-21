7101d230e869de4d2611cec615f4e9441fcc29d9
Simon Bing/Megacity Team <bings@student.ethz.ch>
2018-07-21 12:26:15 +0200

    LQR fix

--- CSII/Exercises/HWExercise2/controller-1.py
+++ CSII/Exercises/HWExercise2/controller-1.py
@@ -5,11 +5,9 @@ import control
 class Controller():
 
     def __init__(self):
-
-        #Define cost matrices here:
-        #defnfine matrices with: self.A = numpy.array([[a11, a12], [a21, a22]])
-
-        #TODO
+        # Gains for controller
+        self.q = numpy.array([[70., 0], [0, 3.]])
+        self.r = numpy.array([0.007])
 
     # Inputs:   d_est   Estimation of distance from lane center (positve when
     #                   offset to the left of driving direction) [m]
@@ -19,20 +17,18 @@ class Controller():
     #           v_ref   Reference of velocity [m/s]
     #           t_delay Delay it took from taking image up to now [s]
     #           dt_last Time it took from last processing to current [s]
-
     # Output:   v_out       velocity of Duckiebot [m/s]
     #           omega_out   angular velocity of Duckiebot [rad/s]
-
     def getControlOutput(self, d_est, phi_est, d_ref, phi_ref, v_ref, t_delay, dt_last):
         #Exception handler for dt_last==0
-        if (dt_last==0) : dt_last=0.0001
-        #Your code goes here: 
-
-        #TODO
-
-        v_out = v_ref       #Change the speed of the bot here.
-
-        #Calculate control input omega_out
-        omega_out = #TODO
-
+        if(dt_last==0):
+            dt_last=0.0001
+        x = numpy.array([[d_est],[phi_est]])
+        #Discrete State Space Matrices
+        a = numpy.array([[1., v_ref*dt_last], [0, 1.]])
+        b = numpy.array([[v_ref*dt_last*dt_last/2], [dt_last]])
+        #Solve Discrete Algebraic Riccati Equation
+        (x_ric, l, g) = control.dare(a, b, self.q, self.r)
+        v_out = v_ref
+        omega_out = -numpy.dot(g,x)
         return (v_out, omega_out)
