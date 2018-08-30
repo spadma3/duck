932e7a84bf677d36c46f56027f5eff5a7279109e
jukindle <gibernas@ethz.ch>
2018-08-30 16:38:28 +0200

    Read Tiles from map data

--- catkin_ws/src/30-localization-and-planning/auto_localization/src/pose_to_tile.py
+++ catkin_ws/src/30-localization-and-planning/auto_localization/src/pose_to_tile.py
@@ -39,23 +39,28 @@ class tilemapping (object):
         self.origin_tag_pos.y = 0
         self.origin_tag_pos.theta = 0 # in degrees
 
-        # load the map file, notice that the file will be overwritten after calibration
+        # Load map file path with ros param set in launch file
         # self.map_filename = rospy.get_param("~map") + ".yaml"
-        # timestr = "{:%Y%m%d-%H%M}".format(datetime.now())
-        # self.output_map_filename = rospy.get_param("~output_file") + timestr + ".yaml"
-        # self.map_data = self.load_map_info(self.map_filename)
+        self.map_filename = "/home/duckietown/duckietown/catkin_ws/src/30-localization-and-planning/auto_localization/config/eth_robotarium.yaml"
+        self.map_data = self.load_map_info()
+        self.tiles = self.map_data['tiles']
+        print self.tiles[1][0]
 
-        #open map_description
-        map_file = open (map_description.py,r)
+
+        # #open map_description
+        # map_file = open (map_description.py,r)
+        # reads data from the map file
+
 
         # Subscribe to global poses
         self.sub_globalposes = rospy.Subscriber("global_poses", GlobalPoseArray, self.callback, queue_size=1)
         print "Init complete"
+        #publish information of current tile
         #self.pub_currenttile = rospy.Publisher("current_tile",GlobalPoseArray,self.callback, queue_size=1)
         #
 
 
-        #  def callback tilepose
+    #  def callback tilepose
     def callback(self,msg):
         tiles_length = 0.585
         msg_tfs = msg.poses
@@ -69,17 +74,27 @@ class tilemapping (object):
         print tile_x
         print tile_y
 
+        # for tile in tiletype:
+        #     print tile['1']
+
         #find the type of the tile from tile numbers in x and y
-        string current_tiletype
-        if tile_x == && tile_y ==
-            current_tiletype = "4-way intersection"
-        elif tile_x == && tile_y ==
-            current_tiletype = "3-way intersection"
-        elif tile_x == && tile_y ==
-            current_tiletype = "curve"
-        elif tile_x == && tile_y ==
-            current_tiletype = "straight"
-        pub_currenttile.publish (current_tiletype)
+        # string current_tiletype
+        # if tile_x == && tile_y ==
+        #     current_tiletype = "4-way intersection"
+        # elif tile_x == && tile_y ==
+        #     current_tiletype = "3-way intersection"
+        # elif tile_x == && tile_y ==
+        #     current_tiletype = "curve"
+        # elif tile_x == && tile_y ==
+        #     current_tiletype = "straight"
+        # pub_currenttile.publish (current_tiletype)
+
+    def load_map_info(self):
+        #map_data = yaml.load(file(rospkg.RosPack().get_path('auto_localization')+"/config/"+self.map_filename,'r')) # Need RosPack get_path to find the file path
+        map_data = yaml.load(file(self.map_filename,'r'))
+        print "Loaded map from file ", self.map_filename
+        # print map_data
+        return map_data
 
 
 
