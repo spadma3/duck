f8857e27d191e092c71dcbf53303dcffe1b1b9fa
breandan <bre@ndan.co>
2018-09-17 02:06:35 -0400

    do not force ROS_MASTER_URI and accept ROS_MASTER env variable

--- docker/env.sh
+++ docker/env.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-IP=$(ifconfig wlan0 | grep "inet " | awk -F'[: ]+' '{ print $4 }')
+if [[ $(ping -c 1 $ROS_MASTER) -eq 0 ]]; then
+    export ROS_MASTER_URI="http://$ROS_MASTER:11311/"
+else
+    IP=$(ifconfig wlan0 | grep "inet " | awk -F'[: ]+' '{ print $4 }')
+    export ROS_MASTER_URI=${ROS_MASTER_URI:-"http://$IP:11311/"}
+    export ROS_IP=${IP}
+fi
+
 source /home/software/environment.sh
 export DUCKIEFLEET_ROOT=/data/config
-export ROS_MASTER_URI=http://${IP}:11311/
-export ROS_IP=${IP}
 export VEHICLE_NAME=$HOSTNAME
 cat misc/duckie.art
 
