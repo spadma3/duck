6b7e71ac47d05b1e1b415860f14ee20971fda2a4
breandan <bre@ndan.co>
2018-09-18 14:53:02 -0400

    use `hostname -I` instead of attempting to parse `ifconfig`

--- docker/env.sh
+++ docker/env.sh
@@ -3,7 +3,7 @@
 if [[ $(ping -c 1 $ROS_MASTER) -eq 0 ]]; then
     export ROS_MASTER_URI="http://$ROS_MASTER:11311/"
 else
-    IP=$(ifconfig wlan0 | grep "inet " | awk -F'[: ]+' '{ print $4 }')
+    IP=$(hostname -I | cut -d " " -f 1)
     export ROS_MASTER_URI=${ROS_MASTER_URI:-"http://$IP:11311/"}
     export ROS_IP=${IP}
 fi
