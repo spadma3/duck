2393253770776ea8f0385bf31c10e24eeebc6f68
hosnerm <merlin.hosner@gmail.com>
2019-02-25 17:24:10 +0100

    changed inverse kinematics node

--- catkin_ws/src/05-teleop/dagu_car/src/inverse_kinematics_node.py
+++ catkin_ws/src/05-teleop/dagu_car/src/inverse_kinematics_node.py
@@ -4,6 +4,7 @@ from duckietown_msgs.msg import WheelsCmdStamped, Twist2DStamped, BoolStamped
 from duckietown_msgs.srv import SetValueRequest, SetValueResponse, SetValue
 from std_srvs.srv import EmptyRequest, EmptyResponse, Empty
 from numpy import *
+import math                                                                     #for the calculations of the wheels command (sqrt, )
 import yaml
 import time
 import os.path
@@ -21,12 +22,16 @@ class InverseKinematicsNode(object):
         # Set parameters using yaml file
         self.readParamFromFile()
 
-        # Set local variable by reading parameters
-        self.gain = self.setup_parameter("~gain", 0.6)
-        self.trim = self.setup_parameter("~trim", 0.0)
-        self.baseline = self.setup_parameter("~baseline", 0.1)
-        self.radius = self.setup_parameter("~radius", 0.0318)
-        self.k = self.setup_parameter("~k", 27.0)
+        # Set local variable by reading parameters (ev. use negative values, because it is easier to debug then)
+        self.gain_dc = self.setup_parameter("~gain_dc", 0.6)                    #changed "gain" to "gain_dc", to identify uniquely for the dc-motor, RFMH_2019_02_25
+        self.trim_dc = self.setup_parameter("~trim_dc", 0.0)                    #changed "trim" to "trim_dc", to identify uniquely for the dc-motor, RFMH_2019_02_25
+        self.gain_servo = self.setup_parameter("~gain_servo", 0.1)              #changed "gain" to "gain_servo", to identify uniquely for the servo-motor, RFMH_2019_02_25
+        self.trim_servo = self.setup_parameter("~trim_servo", 0.0)              #changed "trim" to "trim_servo", to identify uniquely for the servo-motor, RFMH_2019_02_25
+        self.baseline = self.setup_parameter("~baseline", 0.092)                #adjusted car width , RFMH_2019_02_25
+        self.radius = self.setup_parameter("~radius", 0.019)                    #adjusted wheel radius to 0.019m , RFMH_2019_02_25
+        self.k = self.setup_parameter("~k", 27.0)                               #maybe needs adjustment, nothing changed yet , RFMH_2019_02_25
+        self.axis_distance = self.setup_parameter("~axis_distance", 0.105)      #introduce the distance between the axis, RFMH_2019_02_25
+        self.cog_distance = self.setup_parameter("~cog_distance", 0.0525)       #introduce the distance of turning point from the back axis (half the axis_distance), RFMH_2019_02_25
         self.limit = self.setup_parameter("~limit", 1.0)
         self.limit_max = 1.0
         self.limit_min = 0.0
@@ -35,11 +40,15 @@ class InverseKinematicsNode(object):
         self.omega_max = 999.0     # TODO: Calculate v_max !
 
         # Prepare services
-        self.srv_set_gain = rospy.Service("~set_gain", SetValue, self.cbSrvSetGain)
-        self.srv_set_trim = rospy.Service("~set_trim", SetValue, self.cbSrvSetTrim)
+        self.srv_set_gain_dc = rospy.Service("~set_gain_dc", SetValue, self.cbSrvSetGainDc)             #adjust gain to dc-motor, RFMH_2019_02_25
+        self.srv_set_trim_dc = rospy.Service("~set_trim_dc", SetValue, self.cbSrvSetTrimDc)             #adjust trim to dc-motor, RFMH_2019_02_25
+        self.srv_set_gain_servo = rospy.Service("~set_gain_servo", SetValue, self.cbSrvSetGainServo)    #create new service tor the callback function for gain_servo, RFMH_2019_02_25
+        self.srv_set_trim_servo = rospy.Service("~set_trim_servo", SetValue, self.cbSrvSetTrimServo)    #create new service for the callback function for trim_servo, RFMH_2019_02_25
         self.srv_set_baseline = rospy.Service("~set_baseline", SetValue, self.cbSrvSetBaseline)
         self.srv_set_radius = rospy.Service("~set_radius", SetValue, self.cbSrvSetRadius)
         self.srv_set_k = rospy.Service("~set_k", SetValue, self.cbSrvSetK)
+        self.srv_set_axis_distance = rospy.Service("~set_axis_distance", SetValue, self.cbSrvSetAxisDistance)       #create new service for the callback function for axis_distance, RFMH_2019_02_25
+        self.srv_set_cog_distance = rospy.Service("~set_cog_distance", SetValue, self.cbSrvSetCogDistance)          #create new service for the callback function for axis_distance, RFMH_2019_02_25
         self.srv_set_limit = rospy.Service("~set_limit", SetValue, self.cbSrvSetLimit)
         self.srv_save = rospy.Service("~save_calibration", Empty, self.cbSrvSaveCalibration)
 
@@ -78,7 +87,7 @@ class InverseKinematicsNode(object):
         if yaml_dict is None:
             # Empty yaml file
             return
-        for param_name in ["gain", "trim", "baseline", "k", "radius", "limit"]:
+        for param_name in ["gain_dc", "trim_dc", "gain_servo", "trim_servo", "baseline", "radius", "k", "axis_distance", "cog_distance", "limit"]:        #inserted the new parameters defined above, RFMH_2019_02_25
             param_value = yaml_dict.get(param_name)
             if param_name is not None:
                 rospy.set_param("~"+param_name, param_value)
@@ -96,11 +105,15 @@ class InverseKinematicsNode(object):
         # Write to yaml
         data = {
             "calibration_time": time.strftime("%Y-%m-%d-%H-%M-%S"),
-            "gain": self.gain,
-            "trim": self.trim,
+            "gain_dc": self.gain_dc,                                            #the new parameters for the dc-motor and the serve need to be written to the file as well
+            "trim_dc": self.trim_dc,
+            "gain_servo": self.gain_servo,
+            "trim_servo": self.trim_servo,
             "baseline": self.baseline,
             "radius": self.radius,
             "k": self.k,
+            "axis_distance": self.axis_distance,                                #the two new parameters must be written to the yaml-file again, RFMH_2019_02_25
+            "cog_distance": self.cog_distance,
             "limit": self.limit,
         }
 
@@ -116,16 +129,32 @@ class InverseKinematicsNode(object):
         self.saveCalibration()
         return EmptyResponse()
 
-    def cbSrvSetGain(self, req):
-        self.gain = req.value
+    def cbSrvSetGainDc(self, req):                                              #adjusted method name to "cbSrvSetGainDc", RFMH_2019_02_25
+        self.gain_dc = req.value                                                #adjusted gain name to "gain_dc", RFMH_2019_02_25
         self.printValues()
         self.msg_actuator_limits.v = self.v_max     # TODO: Calculate v_max !
         self.msg_actuator_limits.omega = self.omega_max     # TODO: Calculate omega_max !
         self.pub_actuator_limits.publish(self.msg_actuator_limits)
         return SetValueResponse()
 
-    def cbSrvSetTrim(self, req):
-        self.trim = req.value
+    def cbSrvSetTrimDc(self, req):                                              #adjusted method name to "cbSrvSetTrimDc", RFMH_2019_02_25
+        self.trim_dc = req.value                                                #adjusted trim name to "trim_dc", RFMH_2019_02_25
+        self.printValues()
+        self.msg_actuator_limits.v = self.v_max     # TODO: Calculate v_max !
+        self.msg_actuator_limits.omega = self.omega_max     # TODO: Calculate omega_max !
+        self.pub_actuator_limits.publish(self.msg_actuator_limits)
+        return SetValueResponse()
+
+    def cbSrvSetGainServo(self, req):
+        self.gain_servo = req.value
+        self.printValues()
+        self.msg_actuator_limits.v = self.v_max     # TODO: Calculate v_max !
+        self.msg_actuator_limits.omega = self.omega_max     # TODO: Calculate omega_max !
+        self.pub_actuator_limits.publish(self.msg_actuator_limits)
+        return SetValueResponse()
+
+    def cbSrvSetTrimServo(self, req):
+        self.trim_servo = req.value
         self.printValues()
         self.msg_actuator_limits.v = self.v_max     # TODO: Calculate v_max !
         self.msg_actuator_limits.omega = self.omega_max     # TODO: Calculate omega_max !
@@ -156,6 +185,22 @@ class InverseKinematicsNode(object):
         self.pub_actuator_limits.publish(self.msg_actuator_limits)
         return SetValueResponse()
 
+    def cbSrvSetAxisDistance(self, req):                                        #Created two new methods for the parameters "axis_distance" and "cog_distance" to be set. RFMH_2019_02_25
+        self.axis_distance = req.value
+        self.printValues()
+        self.msg_actuator_limits.v = self.v_max     # TODO: Calculate v_max !
+        self.msg_actuator_limits.omega = self.omega_max     # TODO: Calculate omega_max !
+        self.pub_actuator_limits.publish(self.msg_actuator_limits)
+        return SetValueResponse()
+
+    def cbSrvSetCogDistance(self, req):
+        self.cog_distance = req.value
+        self.printValues()
+        self.msg_actuator_limits.v = self.v_max     # TODO: Calculate v_max !
+        self.msg_actuator_limits.omega = self.omega_max     # TODO: Calculate omega_max !
+        self.pub_actuator_limits.publish(self.msg_actuator_limits)
+        return SetValueResponse()
+
     def cbSrvSetLimit(self, req):
         self.limit = self.setLimit(req.value)
         self.printValues()
@@ -175,39 +220,39 @@ class InverseKinematicsNode(object):
             limit = value
         return limit
 
-    def printValues(self):
-        rospy.loginfo("[%s] gain: %s trim: %s baseline: %s radius: %s k: %s limit: %s" % (self.node_name, self.gain, self.trim, self.baseline, self.radius, self.k, self.limit))
+    def printValues(self):                                                      #adjust the output to all the new values as well in the log info, RFMH_2019_02_25
+        rospy.loginfo("[%s] gain_dc: %s trim_dc: %s gain_servo: %s trim_servo: %s baseline: %s radius: %s k: %s axis_distance: %s cog_distance: %s limit: %s" % (self.node_name, self.gain_dc, self.trim_dc, self.gain_servo, self.trim_servo, self.baseline, self.radius, self.k, self.axis_distance, self.cog_distance, self.limit))
 
     def car_cmd_callback(self, msg_car_cmd):
         if not self.actuator_limits_received:
             self.pub_actuator_limits.publish(self.msg_actuator_limits)
 
         # assuming same motor constants k for both motors
-        k_r = self.k
-        k_l = self.k
+        k_wheel = self.k                                                        #changed "k_r" to "k_wheel", because there is only one such konstant need now, RFMH_2019_02_25
+        #k_l = self.k
 
         # adjusting k by gain and trim
-        k_r_inv = (self.gain + self.trim) / k_r
-        k_l_inv = (self.gain - self.trim) / k_l
+        k_wheel_inv = (self.gain + self.trim) / k_wheel                             #changed "k_r" to "k_wheel", RFMH_2019_02_25
+        #k_l_inv = (self.gain - self.trim) / k_l
 
-        omega_r = (msg_car_cmd.v + 0.5 * msg_car_cmd.omega * self.baseline) / self.radius
-        omega_l = (msg_car_cmd.v - 0.5 * msg_car_cmd.omega * self.baseline) / self.radius
+        omega_wheel = msg_car_cmd.v / self.radius                               #omega_r changed to omega_wheel and skipped the whole calculation
+        gamma = pow(pow((msg_car_cmd.v / msg_car_cmd.omega),2.0) - pow(self.cog_distance,2.0),-0.5) * self.axis_distance     #omega_l changed to gamma as this is the steering angle and inserted the new calculation: gamma = f(v, omega)
 
         # conversion from motor rotation rate to duty cycle
         # u_r = (gain + trim) (v + 0.5 * omega * b) / (r * k_r)
-        u_r = omega_r * k_r_inv
+        u_wheel = omega_wheel * k_wheel_inv                                             #omega_r changed to omega_wheel, u_r to u_wheel and k_r_inv to k_wheel_inv, RFMH_2019_02_25
         # u_l = (gain - trim) (v - 0.5 * omega * b) / (r * k_l)
-        u_l = omega_l * k_l_inv
+        #u_l = omega_l * k_l_inv
 
         # limiting output to limit, which is 1.0 for the duckiebot
-        u_r_limited = max(min(u_r, self.limit), -self.limit)
-        u_l_limited = max(min(u_l, self.limit), -self.limit)
+        u_wheel_limited = max(min(u_wheel, self.limit), -self.limit)            #u_r_limited changed to "u_wheel_limited" and "u_r" changed to "u_wheel", RFMH_2019_02_25
+        #u_l_limited = max(min(u_l, self.limit), -self.limit)
 
         # Put the wheel commands in a message and publish
         msg_wheels_cmd = WheelsCmdStamped()
         msg_wheels_cmd.header.stamp = msg_car_cmd.header.stamp
-        msg_wheels_cmd.vel_right = u_r_limited
-        msg_wheels_cmd.vel_left = u_l_limited
+        msg_wheels_cmd.vel_right = u_wheel_limited                              #vel_right is defined in the WheelsCmdStamped --> name needs to be changed everywhere!, RFMH_2019_02_25
+        #msg_wheels_cmd.vel_left = u_l_limited
         self.pub_wheels_cmd.publish(msg_wheels_cmd)
 
     def setup_parameter(self, param_name, default_value):
